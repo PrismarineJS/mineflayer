a326baa4fde70164d146ca5ccfafd0f0f843808a
Josh Wolfe <thejoshwolfe@gmail.com>
2011-01-25 01:31:36 -0700

    better resource extractor interface. ignoring some resources

--- extract_resources.sh
+++ extract_resources.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# TODO: check usage, location, and unzip
+if [ -z $(which unzip) ]
+then
+    echo "you must install unzip"
+    exit
+fi
+if [ "$(basename $1 2>/dev/null)" != "minecraft.jar" ]
+then
+    echo usage:
+    echo $0 path/to/minecraft.jar
+    exit
+fi
 
 resource_names="armor art environment font gui item misc mob terrain terrain.png"
 tmp_stash=$(mktemp -d)
@@ -9,7 +19,7 @@ unzip -q -d $tmp_stash $1
 
 for resource_name in $resource_names
 do
-    cp -r {$tmp_stash,resources}/$resource_name
+    cp -r {$tmp_stash,$(dirname $0)/resources}/$resource_name
 done
 
 rm -rf $tmp_stash
