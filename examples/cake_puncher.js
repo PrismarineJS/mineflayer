6cfc46404c0d461a7d5a2c219bfcceae62058aff
Andrew Kelley <superjoe30@gmail.com>
2011-02-18 03:35:45 -0700

    fix block_finder library

--- examples/cake_puncher.js
+++ examples/cake_puncher.js
@@ -2,16 +2,33 @@ mf.include("block_finder.js");
 mf.include("navigator.js");
 mf.include("auto_respawn.js");
 
+function go() {
+    // FIND CAKE
+    mf.chat("MUST FIND CAKE");
+    var cake_pos = block_finder.findNearest(mf.self().position.floored(), mf.ItemType.Cake_placed);
+    if (cake_pos === undefined) {
+        mf.chat("can't find cake!! :-( :-(((");
+    } else {
+        // STAND ON THE CAKE
+        navigator.navigateTo(cake_pos.offset(0, 0, 1), function() {
+            // PUNCH THE CAKE   
+            mf.startDigging(cake_pos);
+        });
+    }
+}
+
 mf.onChat(function(username, message) {
     if (message == "go") {
-        // FIND CAKE
-        mf.chat("MUST FIND CAKE");
-        var cake_pos = block_finder.findNearest(mf.self().position, mf.ItemType.Cake_placed);
-        if (cake_pos === undefined) {
-            mf.chat("can't find cake!! :-( :-(((");
-        } else {
-            // STAND ON THE CAKE
-            navigator.navigateTo(cake_pos.offset(0, 0, 1));
-        }
+        go();
     }
+});
+
+mf.onStoppedDigging(function() {
+    mf.chat("take THAT, cake!!");
+
+    go();
+});
+
+mf.onHealthChanged(function() {
+    mf.debug("new health: " + mf.health());
 });
