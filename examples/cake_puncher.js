8a6a0522f8da70f30b45f84ec7803b1fb528338c
Andrew Kelley <superjoe30@gmail.com>
2011-02-18 04:49:33 -0700

    generic block puncher

--- examples/cake_puncher.js
+++ examples/cake_puncher.js
@@ -1,34 +1,39 @@
 mf.include("block_finder.js");
 mf.include("navigator.js");
 mf.include("auto_respawn.js");
+mf.include("items.js");
+mf.include("chat_commands.js");
+mf.include("navigator_3d.js");
+
+var get_it;
 
 function go() {
     // FIND CAKE
-    mf.chat("MUST FIND CAKE");
-    var cake_pos = block_finder.findNearest(mf.self().position.floored(), mf.ItemType.Cake_placed);
+    mf.chat("looking for " + get_it.name);
+    var cake_pos = block_finder.findNearest(mf.self().position.floored(), get_it.id);
     if (cake_pos === undefined) {
-        mf.chat("can't find cake!! :-( :-(((");
+        mf.chat("can't find " + get_it.name);
     } else {
         // STAND ON THE CAKE
         navigator.navigateTo(cake_pos.offset(0, 0, 1), function() {
             // PUNCH THE CAKE   
             mf.startDigging(cake_pos);
-        });
+        }, 1);
     }
 }
 
-mf.onChat(function(username, message) {
-    if (message == "go") {
-        go();
-    }
-});
-
 mf.onStoppedDigging(function() {
-    mf.chat("take THAT, cake!!");
+    mf.chat("take THAT "+ get_it.name + "!!");
 
     go();
 });
 
-mf.onHealthChanged(function() {
-    mf.debug("new health: " + mf.health());
-});
+chat_commands.registerCommand("punch", function(username, args) {
+    var ret = items.lookupItemType(args[0]);
+    if (ret.length != 1) {
+        mf.chat("what. " + ret.mapped(function(item){return item.name;}));
+        return;
+    }
+    get_it = ret[0];
+    go();
+}, 1); 
