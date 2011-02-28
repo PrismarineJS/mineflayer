e8b1eb978c295e740a0c331083da00e90578a607
Josh Wolfe <thejoshwolfe@gmail.com>
2011-02-27 23:26:52 -0700

    fixing navigator_3d's stop function

--- examples/navigator_3d.js
+++ examples/navigator_3d.js
@@ -33,9 +33,9 @@ mf.include("quitter.js");
         goToPoint(location.point, undefined, responder_func);
     }, 1);
 
+    var current_checker_interval_id;
     function goToPoint(end, end_radius, responder_func) {
         responder_func("looking for a path from " + mf.self().position.floored() + " to " + end + "...");
-        var checker_interval_id;
         navigator.navigateTo(end, {
             "end_radius": end_radius,
             "timeout_milliseconds": 10 * 1000,
@@ -45,11 +45,12 @@ mf.include("quitter.js");
             "path_found_func": function(path) {
                 responder_func("i can get there in " + path.length + " moves");
                 var previous_position;
-                checker_interval_id = mf.setInterval(function() {
+                current_checker_interval_id = mf.setInterval(function() {
                     var current_position = mf.self().position;
                     if (previous_position !== undefined && current_position.distanceTo(previous_position) < 1) {
                         // i'm stuck
-                        mf.clearInterval(checker_interval_id);
+                        mf.clearInterval(current_checker_interval_id);
+                        current_checker_interval_id = undefined;
                         goToPoint(end, end_radius, responder_func);
                         return;
                     }
@@ -58,12 +59,18 @@ mf.include("quitter.js");
             },
             "arrived_func": function() {
                 responder_func("i have arrived");
-                mf.clearInterval(checker_interval_id);
+                mf.clearInterval(current_checker_interval_id);
+                current_checker_interval_id = undefined;
             },
         });
     }
 
     // stop
-    chat_commands.registerCommand("stop", navigator.stop);
+    chat_commands.registerCommand("stop", function() {
+        navigator.stop();
+        if (current_checker_interval_id !== undefined) {
+            mf.clearInterval(current_checker_interval_id);
+        }
+    });
 })();
 
