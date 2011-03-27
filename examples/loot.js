54c43d9167bd02836d8da01d58df49335986e492
Casey Kuball <darthfett@gmail.com>
2011-03-27 16:06:25 -0400

    First working version of loot.js

--- examples/loot.js
+++ examples/loot.js
@@ -19,6 +19,134 @@ var reset = function() {
     location_string = undefined;
 };
 
+chat_commands.registerCommand("stop",reset);
+
+mf.onWindowOpened(function(window_type) {
+    if (window_type !== mf.WindowType.Chest) {
+        return;
+    }
+    if (respond === undefined || location_string === undefined || chest_position === undefined) {
+        return;
+    }
+
+    if (item_type !== undefined) {
+        if (item_count === undefined) {
+            item_count = inventory.condensedSnapshot(inventory.ChestFull)[item_type];
+            if (item_count === undefined) {
+                respond("I couldn't find any " + items.nameForId(item_type) + ".  Done!");
+                reset();
+                mf.closeWindow();
+                return;
+            }
+        } else {
+            var actual_count = inventory.condensedSnapshot(inventory.ChestFull)[item_type];
+            if (actual_count === undefined) {
+                respond("I couldn't find any " + items.nameForId(item_type) + ".  Done!");
+                reset();
+                mf.closeWindow();
+                return;
+            } else if (actual_count < item_count) {
+                respond("I only see " + actual_count + " " + items.nameForId(item_type) + ".  I'm only going to be able to loot that many.");
+            }
+        }
+        
+    } else {
+        item_count = 0;//1;
+        var counts = inventory.condensedSnapshot(inventory.ChestFull);
+        for (var type in counts) {
+            if (counts.hasOwnProperty(type)) {
+                item_count += counts[type];
+            }
+        }
+        if (item_count === 0) {
+            respond("Done!");
+            reset();
+            mf.closeWindow();
+            return;
+        }
+    }
+    // Deposit items
+    for (var chest_slot = inventory.ChestFull.firstSlot; chest_slot <= inventory.ChestFull.lastSlot; chest_slot++) {
+        var chest_item = inventory.inventoryItem(chest_slot,inventory.ChestFull);
+        if (chest_item.type === mf.ItemType.NoItem) {
+            continue;
+        }
+        if (item_type !== undefined) {
+            if (item_type !== chest_item.type) {
+                continue;
+            }
+        }
+
+        var slots = inventory.slotsForItem(inventory.inventoryItem(chest_slot,inventory.ChestFull).type, inventory.InventoryFull);
+
+        for (var k = 0; k < slots.length; k++) {
+            var inv_slot = slots[k];
+            var inv_item = inventory.inventoryItem(inv_slot,inventory.InventoryFull);
+            var open_slots = mf.itemStackHeight(chest_item.type) - inv_item.count;
+            if (open_slots === 0) {
+                continue;
+            }
+            if (open_slots <= item_count) {
+                //Fill all slots
+                mf.clickUniqueSlot(chest_slot,mf.MouseButton.Left);
+                mf.clickInventorySlot(inv_slot,mf.MouseButton.Left);
+                mf.clickUniqueSlot(chest_slot,mf.MouseButton.Left);
+                chest_item = inventory.inventoryItem(chest_slot, inventory.ChestFull);
+                item_count -= Math.min(open_slots, chest_item.count);
+                if (chest_item.type === mf.ItemType.NoItem) {
+                    break;
+                }
+            } else {
+                //Fill up to item_count slots
+                if (chest_item.count > item_count) {
+                    //Fill item_count slots
+
+                    if (Math.ceil(chest_item.count/2) >= item_count) {
+                        if (Math.ceil(chest_item.count/2) === item_count) {
+                            mf.clickUniqueSlot(chest_slot,mf.MouseButton.Right);
+                            mf.clickInventorySlot(inv_slot,mf.MouseButton.Left);
+                            chest_item = inventory.inventoryItem(chest_slot, inventory.ChestFull);
+                        } else {
+                            mf.clickUniqueSlot(chest_slot,mf.MouseButton.Right);
+                            for (var i = 0; i < item_count; i++) {
+                                mf.clickInventorySlot(inv_slot,mf.MouseButton.Right);
+                            }
+                            mf.clickUniqueSlot(chest_slot,mf.MouseButton.Left);
+                            chest_item = inventory.inventoryItem(chest_slot, inventory.ChestFull);
+                        }
+                    } else {
+                        mf.clickUniqueSlot(chest_slot,mf.MouseButton.Left);
+                        for (var i = 0; i < item_count; i++) {
+                            mf.clickInventorySlot(inv_slot,mf.MouseButton.Right);
+                        }
+                        mf.clickUniqueSlot(chest_slot,mf.MouseButton.Left);
+                        chest_item = inventory.inventoryItem(chest_slot, inventory.ChestFull);
+                    }                    
+                    item_count = 0;
+                    break;
+                } else {
+                    //Fill chest_item.count slots
+                    mf.clickUniqueSlot(chest_slot, mf.MouseButton.Left);
+                    mf.clickInventorySlot(inv_slot,mf.MouseButton.Left);
+                    mf.clickUniqueSlot(chest_slot, mf.MouseButton.Left);
+                    chest_item = inventory.inventoryItem(chest_slot, inventory.ChestFull);
+                    item_count -= chest_item.count;
+                }
+            }
+        }
+        if (item_count === 0) {
+            break;
+        }
+    }
+    if (item_count !== 0 && (item_type === undefined || inventory.itemSlot(item_type,inventory.ChestFull) !== undefined)) {
+       respond("My inventory is full!");
+    } else {
+        respond("Done!");
+    }
+    reset();
+    mf.closeWindow();
+});
+
 var loot = function() {
     if (mf.blockAt(chest_position).type !== mf.ItemType.Chest) {
         if (mf.self().position.floored().distanceTo(chest_position.floored()) > 64) {
@@ -50,15 +178,31 @@ var loot = function() {
             });
         }
     } else {
-        respond("I WOULD be stealing from this chest if chest-looting was implemented!");
-        reset();
+        if (mf.self().position.floored().distanceTo(chest_position.floored()) < 4) {         
+            mf.hax.activateBlock(chest_position);
+        } else {
+            navigator.navigateTo(chest_position, {
+                end_radius: 3,
+                timeout_milliseconds: 1000 * 5,
+                arrived_func: loot,
+                cant_find_func: function() {
+                    respond("I can't get close enough to the chest at " + chest_position + ".");
+                    reset();
+                }
+            });
+        }
     }
 };
 
 chat_commands.registerCommand("loot",function(speaker,args,responder_fun) {
     if (args.length > 0) {
         if (! isNaN(args[0])) {
-            item_count = args.shift();
+            item_count = Math.ceil(args.shift());
+            if (item_count <= 0) {
+                responder_fun("I can't loot " + item_count + " of ANY item!");
+                reset();
+                return;
+            }
         }
     }
     if (args.length > 0) {
@@ -127,9 +271,6 @@ chat_commands.registerCommand("loot",function(speaker,args,responder_fun) {
             reset();
             return;
         } else {
-            mf.debug(typeof item_count);
-            mf.debug(item_count);
-            mf.debug(items.nameForId(item_type));
             responder_fun("I'm going to go loot " + item_count + " " + items.nameForId(item_type) + " from the chest nearest " + location_string);
         }
     } else {
