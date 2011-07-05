39718c6c0be518bb5604394f47868f34aa0b0c0b
Darthfett <darthfett@gmail.com>
2011-07-05 15:26:02 -0700

    cake_puncher no longer crashes when other scripts dig a block

--- mineflayer-script/examples/cake_puncher.js
+++ mineflayer-script/examples/cake_puncher.js
@@ -4,45 +4,45 @@ mf.include("auto_respawn.js");
 mf.include("items.js");
 mf.include("chat_commands.js");
 mf.include("navigator_3d.js");
+function() {
+    var get_it;
 
-var get_it;
-
-function go() {
-    if (get_it === undefined) {
-        return;
+    function go() {
+        if (get_it === undefined) {
+            return;
+        }
+        // FIND CAKE
+        mf.chat("looking for " + get_it.name);
+        var cake_pos_arr = block_finder.findNearest(mf.self().position.floored(), get_it.id, 64, 1);
+        if (cake_pos_arr.length === 0) {
+            mf.chat("can't find " + get_it.name);
+        } else {
+            // STAND ON THE CAKE
+            var cake_pos = cake_pos_arr[0];
+            navigator.navigateTo(cake_pos.offset(0, 1, 0), {
+                "end_radius": 1,
+                "arrived_func": function() {
+                    // PUNCH THE CAKE
+                    mf.onStoppedDigging(function got_it() {
+                        mf.removeHandler(mf.onStoppedDigging, got_it);
+                        mf.chat("take THAT "+ get_it.name + "!!");
+                        go();
+                    });
+                    mf.startDigging(cake_pos);
+                },
+            });
+        }
     }
-    // FIND CAKE
-    mf.chat("looking for " + get_it.name);
-    var cake_pos_arr = block_finder.findNearest(mf.self().position.floored(), get_it.id, 64, 1);
-    if (cake_pos_arr.length === 0) {
-        mf.chat("can't find " + get_it.name);
-    } else {
-        // STAND ON THE CAKE
-        var cake_pos = cake_pos_arr[0];
-        navigator.navigateTo(cake_pos.offset(0, 1, 0), {
-            "end_radius": 1,
-            "arrived_func": function() {
-                // PUNCH THE CAKE
-                mf.startDigging(cake_pos);
-            },
-        });
-    }
-}
 
-mf.onStoppedDigging(function() {
-    mf.chat("take THAT "+ get_it.name + "!!");
-
-    go();
-});
-
-chat_commands.registerCommand("punch", function(username, args, responder_func) {
-    get_it = items.findItemTypeUnambiguously(args[0]);
-    if (get_it === undefined) {
-        return;
-    }
-    go();
-}, 1);
-chat_commands.registerCommand("stop", function() {
-    get_it = undefined;
-    mf.stopDigging();
-});
+    chat_commands.registerCommand("punch", function(username, args, responder_func) {
+        get_it = items.findItemTypeUnambiguously(args[0]);
+        if (get_it === undefined) {
+            return;
+        }
+        go();
+    }, 1);
+    chat_commands.registerCommand("stop", function() {
+        get_it = undefined;
+        mf.stopDigging();
+    });
+}();
