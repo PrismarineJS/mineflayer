c0d2e30c9304b3b579d732b22be99bb3db6713ac
Josh Wolfe <thejoshwolfe@gmail.com>
2011-10-31 03:43:08 -0700

    #32 healthreport now gives less spam and more context.

--- examples/health_reporter.js
+++ examples/health_reporter.js
@@ -9,19 +9,46 @@
         responder_func(healthStatusToString());
     });
 
-    var on_health_func;
+    var callback_func;
     chat_commands.registerCommand("healthreport", function(speaker, args, responder_func) {
         var just_turned_off = false;
-        if (on_health_func !== undefined) {
-            mf.removeHandler(mf.onHealthStatusChanged, on_health_func);
-            on_health_func = undefined;
+        if (callback_func !== undefined) {
+            mf.removeHandler(mf.onHealthStatusChanged, callback_func);
+            callback_func = undefined;
             just_turned_off = true;
         }
         if (args[0] === "on") {
-            on_health_func = function() {
-                responder_func(healthStatusToString());
+            var previous_status = mf.healthStatus();
+            callback_func = function() {
+                var current_status = mf.healthStatus();
+                var messages = [];
+                if (current_status.health < previous_status.health) {
+                    messages.push("ouch!");
+                    if (current_status.food < 18) {
+                        messages.push("NOT regenerating!");
+                    } else {
+                        messages.push("but regenerating.");
+                    }
+                }
+                if (current_status.health < 20) {
+                    if (previous_status.food >= 18 && current_status.food < 18) {
+                        messages.push("stopped regenerating!");
+                    } else if (previous_status.food < 18 && current_status.food >= 18) {
+                        messages.push("now regenerating.");
+                    }
+                }
+                if (previous_status.health < 20 && current_status.health === 20) {
+                    messages.push("full health.");
+                }
+                previous_status = current_status;
+                if (messages.length === 0) {
+                    // not worth spamming uninteresting updates.
+                    return;
+                }
+                messages.push(healthStatusToString());
+                responder_func(messages.join(" "));
             };
-            mf.onHealthStatusChanged(on_health_func);
+            mf.onHealthStatusChanged(callback_func);
             responder_func("health report is now ON");
         } else {
             if (just_turned_off) {
