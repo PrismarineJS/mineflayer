03c89767ebc017819d859e09e3e0535353ce4ef0
Andrew Kelley <superjoe30@gmail.com>
2011-11-05 15:32:00 -0700

    fix blockat test so that all tests pass

--- test/blockat.js
+++ test/blockat.js
@@ -4,10 +4,10 @@ mf.setTimeout(function() {
 }, 5000);
 
 mf.onChunkUpdated(function(start, size) {
-    if (start.x === 48 && start.y === 16 && start.z === 0) {
+    if (start.x === 0 && start.y === 0 && start.z === -80) {
         var pt = new mf.Point(0,0,0);
-        for (pt.z = 58; pt.z <= 62; pt.z++) {
-            for (pt.y = start.y; pt.y < start.y+5; pt.y++) {
+        for (pt.y = 58; pt.y <= 62; pt.y++) {
+            for (pt.z = start.z; pt.z < start.z+5; pt.z++) {
                 for (pt.x = start.x; pt.x < start.x+5; pt.x++) {
                     mf.print(mf.blockAt(pt).type + ",");
                 }
