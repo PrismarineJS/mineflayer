47ec04e47df1295be77de4a8818be9a54df89459
Andrew Kelley <superjoe30@gmail.com>
2013-01-10 21:25:37 -0500

    add the rest of the controls

--- examples/jumper.js
+++ examples/jumper.js
@@ -6,5 +6,15 @@ bot.on('chat', function(username, message) {
   if (message === 'jump') {
     bot.setControlState('jump', true);
     bot.setControlState('jump', false);
+  } else if (message === 'forward') {
+    bot.setControlState('forward', true);
+  } else if (message === 'back') {
+    bot.setControlState('back', true);
+  } else if (message === 'left') {
+    bot.setControlState('left', true);
+  } else if (message === 'right') {
+    bot.setControlState('right', true);
+  } else if (message === 'stop') {
+    bot.clearControlStates();
   }
 });
