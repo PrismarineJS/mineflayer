e5d0b884391e5ca8b8765f28ccf8788c91984eb3
Josh Wolfe <thejoshwolfe@gmail.com>
2015-03-25 00:41:14 -0700

    superflat test starts by making sure it has op

--- test/superflat.js
+++ test/superflat.js
@@ -12,6 +12,8 @@ var bot = mineflayer.createBot({
 
 bot.on('login', function() {
   console.log("waiting a second...");
+  // this wait is to get all the window updates out of the way before we start expecting exactly what we cause.
+  // there are probably other updates coming in that we want to get out of the way too, like health updates.
   setTimeout(startTesting, 1000);
 });
 
@@ -36,12 +38,35 @@ function clearInventory(cb) {
   }
 }
 function startTesting() {
-  console.log("starting test");
-  clearInventory(function() {
-    slashGive("dirt", 1, function() {
-      console.log("done");
+  becomeCreative(function() {
+    console.log("starting test");
+    clearInventory(function() {
+      slashGive("dirt", 1, function() {
+        console.log("done");
+      });
     });
   });
+}
+function becomeCreative(cb) {
+  bot.chat("/gamemode creative");
+  bot.on("message", function onMessage(jsonMsg) {
+    switch (jsonMsg.translate) {
+      case "gameMode.changed":
+        // good.
+        bot.removeListener("message", onMessage);
+        return cb();
+      case "commands.generic.permission":
+        sayEverywhere("ERROR: I need to be an op (allow cheats).");
+        bot.removeListener("message", onMessage);
+        return;
+    }
+    console.log("I didn't expect this message:", jsonMsg);
+  });
+}
+
+function sayEverywhere(message) {
+  bot.chat(message);
+  console.log(message);
 }
 
 function slashGive(itemName, count, cb) {
