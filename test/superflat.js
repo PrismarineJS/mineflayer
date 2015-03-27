92d7cfea3357aa2dfd6f81954e23c9acd2acd023
Josh Wolfe <thejoshwolfe@gmail.com>
2015-03-26 19:38:24 -0700

    superflat test: dirt collect test passes. see #203

--- test/superflat.js
+++ test/superflat.js
@@ -48,24 +48,39 @@ function setInventorySlot(targetSlot, item, cb) {
     setImmediate(cb);
   });
 }
-function startTesting() {
-  callbackChain([
-    resetState,
-    function(cb) {
-      console.log("starting test");
-      setInventorySlot(36, new Item(blocksByName.dirt.id, 1, 0), cb);
-    },
-    function(cb) {
-      fly(new Vec3(0, 2, 0), cb);
-    },
-    function(cb) {
-      placeBlock(36, bot.entity.position.plus(new Vec3(0, -2, 0)), cb);
-    },
-    function(cb) {
-      bot.creative.stopFlying();
+var dirtCollectTest = [
+  resetState,
+  function(cb) {
+    console.log("starting dirt collect test");
+    setInventorySlot(36, new Item(blocksByName.dirt.id, 1, 0), cb);
+  },
+  function(cb) {
+    fly(new Vec3(0, 2, 0), cb);
+  },
+  function(cb) {
+    placeBlock(36, bot.entity.position.plus(new Vec3(0, -2, 0)), cb);
+  },
+  clearInventory,
+  function(cb) {
+    bot.creative.stopFlying();
+    waitForFall(cb);
+  },
+  becomeSurvival,
+  function(cb) {
+    // we are bare handed
+    bot.dig(bot.blockAt(bot.entity.position.plus(new Vec3(0, -1, 0))), cb);
+  },
+  function(cb) {
+    // make sure we collected das dirt
+    setTimeout(function() {
+      assert(Item.equal(bot.inventory.slots[36], new Item(blocksByName.dirt.id, 1, 0)));
+      sayEverywhere("dirt collect test: pass");
       cb();
-    },
-  ], function() {
+    }, 1000);
+  },
+];
+function startTesting() {
+  callbackChain(dirtCollectTest, function() {
     console.log("done");
   });
 }
@@ -157,6 +172,18 @@ function waitForChunksToLoad(cb) {
     }
   }
   cb();
+}
+function waitForFall(cb) {
+  assert(!bot.entity.onGround, "waitForFall called when we were already on the ground");
+  var startingPosition = bot.entity.position.clone();
+  bot.on("move", function onMove() {
+    if (bot.entity.onGround) {
+      var distance = startingPosition.distanceTo(bot.entity.position);
+      assert(distance > 0.2, "waitForFall didn't fall very far: " + distance);
+      bot.removeListener("move", onMove);
+      cb();
+    }
+  });
 }
 
 var superflatLayers = [
