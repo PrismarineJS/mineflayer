912e786f310fd62ba7089dc936a7b7f2a56f8c07
Josh Wolfe <thejoshwolfe@gmail.com>
2015-03-26 19:02:37 -0700

    superflat test digs and places blocks around 0,4,0 to initialize a testing area

--- test/superflat.js
+++ test/superflat.js
@@ -3,6 +3,7 @@ var Vec3 = require('vec3').Vec3;
 var mineflayer = require('../');
 
 var Item = mineflayer.Item;
+var Block = mineflayer.Block;
 var blocksByName = mineflayer.ItemIndex.blocksByName;
 
 var bot = mineflayer.createBot({
@@ -36,18 +37,20 @@ function clearInventory(cb) {
 
 // you need to be in creative mode for this to work
 function setInventorySlot(targetSlot, item, cb) {
+  if (Item.equal(bot.inventory.slots[targetSlot], item)) {
+    // already good to go
+    return setImmediate(cb);
+  }
   bot.creative.setInventorySlot(targetSlot, item);
   bot.inventory.once("windowUpdate", function(slot, oldItem, newItem) {
-    //console.log("windowUpdate:", slot, oldItem, newItem);
     assert(slot === targetSlot);
     assert(Item.equal(item, newItem));
-    cb();
+    setImmediate(cb);
   });
 }
 function startTesting() {
   callbackChain([
-    becomeCreative,
-    clearInventory,
+    resetState,
     function(cb) {
       console.log("starting test");
       setInventorySlot(36, new Item(blocksByName.dirt.id, 1, 0), cb);
@@ -56,7 +59,7 @@ function startTesting() {
       fly(new Vec3(0, 2, 0), cb);
     },
     function(cb) {
-      placeBlock(36, new Vec3(0, -2, 0), cb);
+      placeBlock(36, bot.entity.position.plus(new Vec3(0, -2, 0)), cb);
     },
     function(cb) {
       bot.creative.stopFlying();
@@ -66,10 +69,27 @@ function startTesting() {
     console.log("done");
   });
 }
-function becomeCreative(cb) {
+
+function resetState(cb) {
+  callbackChain([
+    becomeCreative,
+    clearInventory,
+    function(cb) {
+      bot.creative.startFlying();
+      teleport(new Vec3(0, 4, 0), cb);
+    },
+    waitForChunksToLoad,
+    resetBlocksToSuperflat,
+    clearInventory,
+  ], cb);
+}
+
+function becomeCreative(cb) { setCreativeMode(true, cb); }
+function becomeSurvival(cb) { setCreativeMode(false, cb); }
+function setCreativeMode(value, cb) {
   // this function behaves the same whether we start in creative mode or not.
   // also, creative mode is always allowed for ops, even if server.properties says force-gamemode=true in survival mode.
-  bot.chat("/gamemode creative");
+  bot.chat("/gamemode " + (value ? "creative" : "survival"));
   bot.on("message", function onMessage(jsonMsg) {
     switch (jsonMsg.translate) {
       case "gameMode.changed":
@@ -90,16 +110,101 @@ function becomeCreative(cb) {
 function fly(delta, cb) {
   bot.creative.flyTo(bot.entity.position.plus(delta), cb);
 }
-function placeBlock(slot, delta, cb) {
+function placeBlock(slot, position, cb) {
   bot.setQuickBarSlot(slot - 36);
-  var position = bot.entity.position.plus(delta);
-  bot.placeBlock(bot.blockAt(position), delta.scaled(-1));
+  // always place the block on the top of the block below it, i guess.
+  var referenceBlock = bot.blockAt(position.plus(new Vec3(0, -1, 0)));
+  bot.placeBlock(referenceBlock, new Vec3(0, 1, 0));
   setImmediate(cb);
+}
+function teleport(position, cb) {
+  bot.chat("/tp " + bot.username + " " + position.x + " " + position.y + " " + position.z);
+  bot.on("move", function onMove() {
+    if (bot.entity.position.distanceTo(position) < 0.9) {
+      // close enough
+      bot.removeListener("move", onMove);
+      cb();
+    }
+  });
 }
 
 function sayEverywhere(message) {
   bot.chat(message);
   console.log(message);
+}
+
+var deltas3x3 = [
+  new Vec3(-1, 0, -1),
+  new Vec3( 0, 0, -1),
+  new Vec3( 1, 0, -1),
+  new Vec3(-1, 0,  0),
+  new Vec3( 0, 0,  0),
+  new Vec3( 1, 0,  0),
+  new Vec3(-1, 0,  1),
+  new Vec3( 0, 0,  1),
+  new Vec3( 1, 0,  1),
+];
+
+function waitForChunksToLoad(cb) {
+  var isLoaded = true;
+  // check 3x3 chunks around us
+  for (var i = 0; i < deltas3x3.length; i++) {
+    if (bot.blockAt(bot.entity.position.plus(deltas3x3[i].scaled(64))) == null) {
+      // keep wait
+      return setTimeout(function() {
+        waitForChunksToLoad(cb);
+      }, 100);
+    }
+  }
+  cb();
+}
+
+var superflatLayers = [
+  new Block(blocksByName.bedrock.id),
+  new Block(blocksByName.dirt.id),
+  new Block(blocksByName.dirt.id),
+  new Block(blocksByName.grass.id),
+  // and then air
+];
+function resetBlocksToSuperflat(cb) {
+  var groundY = 4;
+  for (var y = groundY + 4; y >= groundY - 1; y--) {
+    var expectedBlock = superflatLayers[y];
+    for (var i = 0; i < deltas3x3.length; i++) {
+      var position = bot.entity.position.plus(deltas3x3[i]);
+      position.y = y;
+      var block = bot.blockAt(position);
+      if (expectedBlock == null) {
+        if (block.name === "air") continue;
+        // dig it
+        return digAndResume(position);
+      } else {
+        if (expectedBlock.type === block.type) continue;
+        // fix it
+        if (block.type !== 0) {
+          // dig it
+          return digAndResume(position);
+        }
+        // place it
+        return placeAndResume(position, expectedBlock);
+      }
+    }
+  }
+  // all good
+  cb();
+
+  function digAndResume(position) {
+    bot.dig(bot.blockAt(position), resume);
+  }
+  function placeAndResume(position, block) {
+    setInventorySlot(36, new Item(block.type, 1, 0), function() {
+      placeBlock(36, position);
+      resume();
+    });
+  }
+  function resume() {
+    resetBlocksToSuperflat(cb);
+  }
 }
 
 function callbackChain(functions, cb) {
