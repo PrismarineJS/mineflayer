cbc373550a1316b3af3d993a1216297c0cbb3b57
Pietro <pietro210@hotmail.it>
2015-05-28 19:08:59 +0200

    Add description to echo.js

--- examples/echo.js
+++ examples/echo.js
@@ -1,3 +1,11 @@
+/*
+ * This is one of the simplest examples.
+ *
+ * We created a simple bot that echoes back everything that is said on chat.
+ *
+ * It's not very useful yet, but you can use this as a starting point
+ * to create your own bot.
+ */
 var mineflayer = require('../');
 
 if(process.argv.length < 4 || process.argv.length > 6) {
