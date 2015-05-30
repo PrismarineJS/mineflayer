b0847aab51bb02b21fc5bec275e8ae5e52f95b60
Pietro <pietro210@hotmail.it>
2015-05-30 07:18:31 +0200

    Improve digger.js

--- examples/digger.js
+++ examples/digger.js
@@ -1,3 +1,15 @@
+/*
+ * Never spend hours mining from ground to bedrock again!
+ *
+ * Learn how to create a simple bot that is capable of digging the block
+ * below his feet and then going back up by creating a dirt column to the top.
+ *
+ * As always, you can send the bot commands using chat messages, and monitor
+ * his inventory at any time.
+ *
+ * Remember that in survival mode he might not have enough dirt to get back up,
+ * so be sure to teach him a few more tricks before leaving him alone at night.
+ */
 var mineflayer = require('../');
 
 if(process.argv.length < 4 || process.argv.length > 6) {
@@ -14,33 +26,39 @@ var bot = mineflayer.createBot({
 });
 
 bot.on('chat', function(username, message) {
-  if(message === 'debug') {
-    console.log(bot.inventory);
-  } else if(message === 'dig') {
-    dig();
-  } else if(message === 'build') {
-    build();
-  } else if(message === 'list') {
-    listInventory();
-  } else if(message === 'dirt') {
-    bot.equip(0x03, 'hand', function(err) {
-      if(err) {
-        console.error(err.stack);
-        bot.chat("unable to equip dirt");
-      } else {
-        bot.chat("equipped dirt");
-      }
-    });
-  } else if(message === 'creative') {
-    bot.chat("/gamemode 1");
-  } else if(message === 'survival') {
-    bot.chat("/gamemode 0");
+  if (username === bot.username) return;
+  switch(message) {
+    case 'list':
+      listInventory();
+      break;
+    case 'dig':
+      dig();
+      break;
+    case 'build':
+      build();
+      break;
+    case 'equip dirt':
+      equipDirt();
+      break;
   }
 });
 
-bot.on('game', function() {
-  bot.chat("game mode is " + bot.game.gameMode);
-});
+function listInventory() {
+  var id, count, item, idx, slot;
+  var output = "";
+  for(idx in bot.inventory.slots) {
+    slot = bot.inventory.slots[idx];
+    if(slot == null) continue;
+    count = slot.count;
+    id = slot.type;
+    item = mineflayer.data.findItemOrBlockById(id);
+    if(count) output += item.name + ": " + count + ", ";
+  }
+  if(!output)
+    bot.chat('I have no items');
+  else
+    bot.chat(output);
+}
 
 function dig() {
   if(bot.targetDigBlock) {
@@ -75,19 +93,13 @@ function build() {
   }
 }
 
-function listInventory() {
-  var id, count, item, idx, slot;
-  var output = "";
-  for(idx in bot.inventory.slots) {
-    slot = bot.inventory.slots[idx];
-    if(slot == null) continue;
-    count = slot.count;
-    id = slot.type;
-    item = mineflayer.data.findItemOrBlockById(id);
-    if(count) output += item.name + ": " + count + ", ";
-  }
-  if(!output)
-    bot.chat('I have no items');
-  else
-    bot.chat(output);
+function equipDirt() {
+  bot.equip(0x03, 'hand', function(err) {
+    if(err) {
+      console.error(err.stack);
+      bot.chat("unable to equip dirt");
+    } else {
+      bot.chat("equipped dirt");
+    }
+  });
 }
