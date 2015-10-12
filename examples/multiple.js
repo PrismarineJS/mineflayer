3dbc1cf61cca2c65ea2c98776aa0b2f35bba4fb0
Romain Beaumont <romain.rom1@gmail.com>
2015-10-12 23:37:28 +0200

    add simple example to run multiple bots

---
+++ examples/multiple.js
@@ -0,0 +1,27 @@
+var mineflayer = require('../');
+
+if(process.argv.length < 3 || process.argv.length > 5) {
+  console.log("Usage : node multiple.js <host> <port>");
+  process.exit(1);
+}
+
+var i=0;
+function next() {
+  if(i<10) {
+    i++;
+    setTimeout(function () {
+      createBot("mineflayer-bot" + i);
+      next();
+    }, 100);
+  }
+}
+next();
+
+function createBot(name)
+{
+  var bot = mineflayer.createBot({
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    username: name
+  });
+}
