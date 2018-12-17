b259a49da475e062bfbea27d369b5119ff15cc3d
Romain Beaumont <romain.rom1@gmail.com>
2018-12-17 03:13:17 +0100

    test lastest sub versions instead of first

--- test/externalTest.js
+++ test/externalTest.js
@@ -30,7 +30,7 @@ const download = require('minecraft-wrap').download
 const MC_SERVER_PATH = path.join(__dirname, 'server')
 
 const { firstVersion, lastVersion } = require('./common/parallel')
-mineflayer.supportedVersions.forEach((supportedVersion, i) => {
+mineflayer.testedVersions.forEach((supportedVersion, i) => {
   if (!(i >= firstVersion && i <= lastVersion)) {
     return
   }
