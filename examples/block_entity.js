9a9a96816819dcf8836e529f2472eda64cecbf40
Kasper Seweryn <casper@wvffle.net>
2019-05-02 21:57:05 +0200

    Add block entity example

---
+++ examples/block_entity.js
@@ -0,0 +1,39 @@
+/*
+ * This example demonstrates how easy it is to create a bot
+ * that fetches monster spawners mob type
+ */
+const mineflayer = require('mineflayer')
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node block_entity.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'block_entity',
+  password: process.argv[5],
+  verbose: true
+})
+
+bot.on('message', (cm) => {
+  if (cm.toString().includes('spawner')) {
+    spawner()
+  }
+})
+
+function spawner () {
+  const block = bot.findBlock({
+    matching: 52,
+    point: bot.entity.position,
+  })
+
+  if (!block) {
+    return bot.chat('Monster spawner not found')
+  }
+
+  bot.chat(`Entity type: ${block.blockEntity.SpawnData.id}`)
+  bot.chat(`Delay: ${block.blockEntity.Delay}`)
+  console.log(block.blockEntity)
+}
