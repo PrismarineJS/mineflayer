81de90ff509f68136a20454f548d6ed140bc6e8b
Karang <arthur.hennequin@free.fr>
2020-04-12 19:10:47 +0200

    Start debugging 1.13

--- test/externalTests/fishing.js
+++ test/externalTests/fishing.js
@@ -2,16 +2,21 @@ const assert = require('assert')
 
 module.exports = () => (bot, done) => {
   const Item = require('prismarine-item')(bot.version)
+  const mcData = require('minecraft-data')(bot.version)
+
+  let grassName = 'grass'
+  if (['1.13', '1.13.1', '1.13.2'].includes(bot.version)) {
+    grassName = 'grass_block'
+  }
 
   bot.test.sayEverywhere('/weather thunder')
   bot.test.sayEverywhere('/fill ~-5 ~-1 ~-5 ~5 ~-1 ~5 water')
-  bot.test.setInventorySlot(36, new Item(346, 1, 0), (err) => {
+  bot.test.setInventorySlot(36, new Item(mcData.itemsByName.fishing_rod.id, 1, 0), (err) => {
     assert.ifError(err)
-
     bot.lookAt(bot.entity.position, true, () => {
       bot.fish((err) => {
         setTimeout(() => {
-          bot.test.sayEverywhere('/fill ~-5 ~-1 ~-5 ~5 ~-1 ~5 grass')
+          bot.test.sayEverywhere('/fill ~-5 ~-1 ~-5 ~5 ~-1 ~5 ' + grassName)
           assert.strictEqual(err, undefined)
         }, 100)
       })
