be8f32a38cb25e809df16ce5d63c422a9b4fb6ca
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-06-02 20:42:52 +0200

    Add auto totem example (#1021)
    * Add auto totem example
    
    * Add missing =
    
    * Some simple fixes
    
    * More fixes
    
    * Delete trailng spaces
    
    * Add ideas from Karang
    
    * Add missing symbol
    
    * Delete trailing space
    
    * Remove trailing space

---
+++ examples/auto_totem.js
@@ -0,0 +1,30 @@
+/*
+ * This script will automaticly set if a totem is in the inventory the totem in the off-hand.
+ * It checks for a totem every secound.
+ */
+const mineflayer = require('mineflayer')
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node echo.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'totem',
+  password: process.argv[5]
+})
+
+bot.on('spawn', () => {
+  const mcData = require('minecraft-data')(bot.version) // You know the version when the bot has spawned
+  const totemId = mcData.itemsByName.totem_of_undying.id // Get the correct id
+  if (mcData.itemsByName.totem_of_undying) {
+    setInterval(() => {
+      var totem = bot.inventory.findInventoryItem(totemId, null)
+      if (totem) {
+        bot.equip(totem, 'off-hand')
+      }
+    }, 50)
+  }
+})
