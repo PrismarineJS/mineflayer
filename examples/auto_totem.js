d5caff9c6d6f5a242fe081f7d4d51ce7666b3444
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-06-02 17:02:28 +0200

    Add auto totem example

---
+++ examples/auto_totem.js
@@ -0,0 +1,24 @@
+/*
+ * This script will automaticly set if a totem is in the inventory the totem in the off-hand.
+ * It checks for a totem every secound.
+ */
+const mineflayer = require('mineflayer')
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node echo.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'totem',
+  password: process.argv[5]
+})
+
+let helpbc = setInterval(() => {  
+  var totem = bot.inventory.findInventoryItem(449, null);
+  if (totem && totem.type == 449) {
+  	bot.equip(totem, "off-hand");
+  }
+}, 1000)
