def8ac8964a409c5a4665ed043664caa89dca445
Nurutomo <tixbig@gmail.com>
2020-07-05 09:11:10 +0700

    Update chat.js

--- test/externalTests/chat.js
+++ test/externalTests/chat.js
@@ -1,9 +1,21 @@
 const assert = require('assert')
 
 module.exports = () => (bot, done) => {
-  bot.chat('/advancement grant @p only minecraft:story/mine_stone')
-  bot.on('message', (json) => {
-    assert.strictEqual(json.toString(), bot.username + ' has made advancement [Stone Age]')
-    done()
-  })
+  if (1.11 < bot.majorVersion) {
+    bot.chat('/advancement grant @p only minecraft:story/mine_stone')
+    bot.on('message', (json) => {
+      const str = json.toString()
+      console.log(str)
+      assert.strictEqual(str, bot.username + ' has made advancement [Stone Age]')
+      done()
+    })
+  } else {
+    bot.chat('/achievement give mminecraft:achievement.openInventory @p')
+    bot.on('message', (json) => {
+      const str = json.toString()
+      console.log(str)
+      assert.strictEqual(str, bot.username + ' has just earned achieviement [Taking Inventoy]')
+      done()
+    })
+  }
 }
