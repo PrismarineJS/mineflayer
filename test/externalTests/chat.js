89fb7e33e4478fd91e754095c8bf4f36aa43eda5
Nurutomo <tixbig@gmail.com>
2020-07-05 13:36:27 +0700

    Update chat.js

--- test/externalTests/chat.js
+++ test/externalTests/chat.js
@@ -1,26 +1,28 @@
 const assert = require('assert')
 
 module.exports = (supportedVersion) => (bot, done) => {
-  const version = parseFloat(supportedVersion.match(/\d+\.(\d+)/)[1])
   setTimeout(() => {
-    if (version < 12) {
-      console.log('/achievement give achievement.openInventory @p')
-      bot.chat('/achievement give achievement.openInventory @p')
-      bot.once('message', (json) => {
-        const str = json.toString()
-        console.log(str)
-        assert.strictEqual(str, bot.username + ' has just earned the achievement [Taking Inventoy]')
+    console.log('/advancement grant @p only minecraft:story/mine_stone')
+    bot.chat('/advancement grant @p only minecraft:story/mine_stone')
+    bot.once('message', (json) => {
+      const str = json.toString()
+      console.log(str)
+      
+      if (/\/help/.test(str)) {
+        // Before 1.12
+        console.log('/achievement give achievement.openInventory @p')
+        bot.chat('/achievement give achievement.openInventory @p')
+        bot.once('message', (json) => {
+          const str = json.toString()
+          console.log(str)
+          
+          assert.ok(str.test(/has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]'))
+          done()
+        })
+      } else {
+        assert.ok(str.test(/has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]'))
         done()
-      })
-    } else {
-      console.log('/advancement grant @p only minecraft:story/mine_stone')
-      bot.chat('/advancement grant @p only minecraft:story/mine_stone')
-      bot.once('message', (json) => {
-        const str = json.toString()
-        console.log(str)
-        assert.strictEqual(str, bot.username + ' has made the advancement [Stone Age]')
-        done()
-      })
-    }
+      }
+    })
   }, 500)
 }
