b5c14f87b1a252ad5f04d0b989cb2b815ee1d40e
Nurutomo <tixbig@gmail.com>
2020-07-05 13:50:38 +0700

    Why 1.8-1.11 fail the test?

--- test/externalTests/chat.js
+++ test/externalTests/chat.js
@@ -2,25 +2,18 @@ const assert = require('assert')
 
 module.exports = (supportedVersion) => (bot, done) => {
   setTimeout(() => {
-    console.log('/advancement grant @p only minecraft:story/mine_stone')
-    bot.chat('/advancement grant @p only minecraft:story/mine_stone')
+    if (/^1.(8|9|10|11)/.test(supportedVersion)) {
+      console.log('/achievement give achievement.openInventory @p')
+      bot.chat('/achievement give achievement.openInventory @p')
+    } else {
+      console.log('/advancement grant @p only minecraft:story/mine_stone')
+      bot.chat('/advancement grant @p only minecraft:story/mine_stone')
+    }
     bot.once('message', (json) => {
       const str = json.toString()
       console.log(str)
-      if (/\/help/.test(str)) {
-        // Before 1.12
-        console.log('/achievement give achievement.openInventory @p')
-        bot.chat('/achievement give achievement.openInventory @p')
-        bot.once('message', (json) => {
-          const str = json.toString()
-          console.log(str)
-          assert.ok(str.test(/has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]/))
-          done()
-        })
-      } else {
-        assert.ok(str.test(/has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]/))
-        done()
-      }
+      assert.ok(str.test(/[\w_]+ has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]$/))
+      done()
     })
   }, 500)
 }
