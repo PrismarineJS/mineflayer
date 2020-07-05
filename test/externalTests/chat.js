87ff290194bd6dbfbbd78c7c7ed46fd48cd51b68
Nurutomo <tixbig@gmail.com>
2020-07-05 14:08:11 +0700

    Update chat.js

--- test/externalTests/chat.js
+++ test/externalTests/chat.js
@@ -2,17 +2,11 @@ const assert = require('assert')
 
 module.exports = (supportedVersion) => (bot, done) => {
   setTimeout(() => {
-    if (/^1.(8|9|10|11)/.test(supportedVersion)) {
-      console.log('/achievement give achievement.openInventory @p')
-      bot.chat('/achievement give achievement.openInventory @p')
-    } else {
-      console.log('/advancement grant @p only minecraft:story/mine_stone')
-      bot.chat('/advancement grant @p only minecraft:story/mine_stone')
-    }
+    bot.chat('/tellraw Nurutomo {"translate":"commands.fill.failed"}')
     bot.once('message', (json) => {
       const str = json.toString()
       console.log(str)
-      assert.ok(str.test(/[\w_]+ has (just earned|made) the (achievement|advancement) \[(Taking Inventoy|Stone Age)\]$/))
+      assert.strictEqual(str, 'No blocks filled')
       done()
     })
   }, 500)
