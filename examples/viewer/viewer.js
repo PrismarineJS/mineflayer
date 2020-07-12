88102f278b93cd2ed0032993df71473d78feec6b
Romain Beaumont <romain.rom1@gmail.com>
2020-07-12 17:52:38 +0200

    add viewer example, fix #1136

---
+++ examples/viewer/viewer.js
@@ -0,0 +1,25 @@
+/*
+ *
+ * A simple bot demo https://github.com/PrismarineJS/prismarine-viewer
+ * Start it then open your browser to http://localhost:3007 and enjoy the view
+ *
+ */
+
+const mineflayer = require('mineflayer')
+const mineflayerViewer = require('prismarine-viewer').mineflayer
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node viewer.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'viewer',
+  password: process.argv[5]
+})
+
+bot.once('spawn', () => {
+  mineflayerViewer(bot, 3007)
+})
