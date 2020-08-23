735da107ea69426d52e534aac0b5efb2d4adaee1
imharvol <ImHarvol@gmail.com>
2020-08-24 01:17:31 +0200

    Fix 'itemDrop' (#1256)
    * Add 'itemDrop' event
    
    * Change 'itemDrop' event condition
    
    * Add tests
    
    Tests aren't passing starting 1.13 due to the entity.name being different than 'item'/'Item'
    
    * 'itemDrop' working from 1.8 to 1.13 included
    
    It's failing because entity.name changes
    
    * Fix "'itemDrop' event" tests
    
    Co-authored-by: Romain Beaumont <romain.rom1@gmail.com>

--- test/internalTest.js
+++ test/internalTest.js
@@ -341,6 +341,7 @@ mineflayer.testedVersions.forEach((supportedVersion, i) => {
           })
         })
       })
+
       it('sets players[player].entity to null upon despawn', (done) => {
         let serverClient = null
         bot.once('entitySpawn', (entity) => {
@@ -427,6 +428,69 @@ mineflayer.testedVersions.forEach((supportedVersion, i) => {
               { type: 0, key: 1, value: 1 }
             ]
           })
+        })
+      })
+
+      it('\'itemDrop\' event', function (done) {
+        const itemData = {
+          itemId: 149,
+          itemCount: 5
+        }
+
+        server.on('login', (client) => {
+          bot.on('itemDrop', (entity) => {
+            const slotPosition = metadataPacket.metadata[0].key
+
+            if (bot.supportFeature('itemsAreAlsoBlocks')) {
+              assert.strictEqual(entity.metadata[slotPosition].blockId, itemData.itemId)
+            } else if (bot.supportFeature('itemsAreNotBlocks')) {
+              assert.strictEqual(entity.metadata[slotPosition].itemId, itemData.itemId)
+            }
+            assert.strictEqual(entity.metadata[slotPosition].itemCount, itemData.itemCount)
+
+            done()
+          })
+
+          let entityType
+          if (['1.8', '1.9', '1.10', '1.11', '1.12'].includes(bot.majorVersion)) {
+            entityType = 2
+          } else {
+            entityType = mcData.entitiesArray.find(e => e.name.toLowerCase() === 'item' || e.name.toLowerCase() === 'item_stack').id
+          }
+          client.write('spawn_entity', {
+            entityId: 16,
+            objectUUID: '00112233-4455-6677-8899-aabbccddeeff',
+            type: Number(entityType),
+            x: 0,
+            y: 0,
+            z: 0,
+            pitch: 0,
+            yaw: 0,
+            objectData: 1,
+            velocityX: 0,
+            velocityY: 0,
+            velocityZ: 0
+          })
+
+          const metadataPacket = {
+            entityId: 16,
+            metadata: [
+              { key: 7, type: 6, value: { itemCount: itemData.itemCount } }
+            ]
+          }
+          // Versions prior to 1.13 use 5 as type field value of metadata for storing a slot. 1.13 and so on, use 6
+          // Also the structure of a slot changes from 1.12 to 1.13
+          if (bot.supportFeature('itemsAreAlsoBlocks')) {
+            metadataPacket.metadata[0].key = 6
+            metadataPacket.metadata[0].type = 5
+            metadataPacket.metadata[0].value.blockId = itemData.itemId
+            metadataPacket.metadata[0].value.itemDamage = 0
+          } else if (bot.supportFeature('itemsAreNotBlocks')) {
+            if (bot.majorVersion === '1.13') metadataPacket.metadata[0].key = 6
+            metadataPacket.metadata[0].value.itemId = itemData.itemId
+            metadataPacket.metadata[0].value.present = true
+          }
+          client.write('entity_metadata', metadataPacket)
         })
       })
     })
