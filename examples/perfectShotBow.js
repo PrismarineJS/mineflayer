7980c780e340165465abd636fd07e1fa213ee3d3
Javier García <javier.garcia@westwing.es>
2020-10-16 00:02:05 +0200

    fixed the linting errors

--- examples/perfectShotBow.js
+++ examples/perfectShotBow.js
@@ -1,30 +1,48 @@
-const mineflayer = require('mineflayer');
-const { hawkEye, getPlayer } = require('minecrafthawkeye');
+const mineflayer = require('mineflayer')
+const minecraftHawkEye = require('minecrafthawkeye')
 
 const bot = mineflayer.createBot({
-    host: process.argv[2],
-    port: parseInt(process.argv[3]),
-    username: process.argv[4] ? process.argv[4] : 'Archer',
-    password: process.argv[5]
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'Archer',
+  password: process.argv[5]
 })
+bot.loadPlugin(minecraftHawkEye)
 
-hawkEye.load(bot);
+bot.on('spawn', function () {
+  bot.chat('/give ' + bot.username + ' bow{Enchantments:[{id:unbreaking,lvl:100}]} 1')
+  bot.chat('/give ' + bot.username + ' minecraft:arrow 300')
+  bot.chat('/time set day')
+  bot.chat('/kill @e[type=minecraft:arrow]')
 
-bot.on('spawn', function() {
-    bot.chat('/give Archer bow{Enchantments:[{id:unbreaking,lvl:100}]} 1');
-    bot.chat('/give Archer minecraft:arrow 300');
-    bot.chat('/time set day');
-    bot.chat('/kill @e[type=minecraft:arrow]');
+  bot.chat('Ready!')
 
-    bot.chat('Ready!');
+  // Get target for block position, use whatever you need
+  const target = bot.hawkEye.getPlayer()
+  console.log(target)
+  if (!target) {
+    return false
+  }
 
-    let target = getPlayer(bot);
-    if (!target)
-        return false;
+  // Auto attack every 1,2 secs until target is dead or is to far away
+  bot.hawkEye.autoAttack(target)
+  // If you force stop attack use:
+  // hawkEye.stop();
 
-    // Auto attack every 1,2 secs until target is dead or is to far away
-    hawkEye.attack(target);
+  // Use one shot time with calc:
+  // bot.hawkEye.oneShot(target);
 
-    // If you force stop attack use:
-    // hawkEye.stop();
-});
+  // If you want to shot in XYZ position:
+  /*
+            const blockPosition = {
+                    position: {
+                        x: 244.5,
+                        y: 75.5,
+                        z: -220
+                    },
+                    isValid: true // Fake to is "alive"
+                }
+            // bot.hawkEye.oneShot(blockPosition);
+            // bot.hawkEye.autoAttack(blockPosition);
+        */
+})
