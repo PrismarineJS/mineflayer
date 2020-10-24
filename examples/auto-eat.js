7b9f57f2ee69345b139fa6fbd143b0a82c0aef88
Link <50463727+LINKdiscordd@users.noreply.github.com>
2020-10-24 13:59:59 +0200

    Added the enable and disable function

--- examples/auto-eat.js
+++ examples/auto-eat.js
@@ -1,27 +1,35 @@
-const mineflayer = require('mineflayer')
-const autoeat = require('mineflayer-auto-eat')
+const mineflayer = require("mineflayer")
+const autoeat = require("mineflayer-auto-eat")
 
 const bot = mineflayer.createBot({
   host: process.argv[2],
   port: process.argv[3],
   username: process.argv[4],
-  password: process.argv[5]
+  password: process.argv[5],
 })
 
-bot.loadPlugin(autoeat) // Load the plugin
+// Load the plugin
+bot.loadPlugin(autoeat)
 
-bot.autoEat = {
-  priority: 'foodPoints', // What the bot should prioritize (options: "foodPoints" or "saturation").
-  startAt: 14, // If the bot reaches this amount of food points the auto eat plugin will start eating.
-  bannedFood: ['golden_apple', 'enchanted_golden_apple'] // Food which the bot should not eat.
-}
-
+bot.once("spawn", () => {
+  bot.autoEat.options = {
+    priority: "foodPoints",
+    startAt: 14,
+    bannedFood: [],
+  }
+})
 // The bot eats food automatically and emits these events when it starts eating and stops eating.
 
-bot.on('autoeat_started', () => {
-  console.log('Auto Eat started!')
+bot.on("autoeat_started", () => {
+  console.log("Auto Eat started!")
 })
 
-bot.on('autoeat_stopped', () => {
-  console.log('Auto Eat stopped!')
+bot.on("autoeat_stopped", () => {
+  console.log("Auto Eat stopped!")
+})
+
+bot.on("health", () => {
+  if (bot.food === 20) bot.autoEat.disable()
+  // Disable the plugin if the bot is at 20 food points
+  else bot.autoEat.enable() // Else enable the plugin again
 })
