c909a60fbf8bdc205f6f44f02a8923e6defcf571
Romain Beaumont <romain.rom1@gmail.com>
2020-12-05 21:33:33 +0100

    jumper

--- examples/jumper.js
+++ examples/jumper.js
@@ -6,6 +6,7 @@
  * This bot can move, jump, ride vehicles, attack nearby entities and much more.
  */
 const mineflayer = require('mineflayer')
+const { on, once } = require('events')
 
 if (process.argv.length < 4 || process.argv.length > 6) {
   console.log('Usage : node jumper.js <host> <port> [<name>] [<password>]')
@@ -21,96 +22,111 @@ const bot = mineflayer.createBot({
 
 let target = null
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  target = bot.players[username].entity
-  let entity
-  switch (message) {
-    case 'forward':
-      bot.setControlState('forward', true)
-      break
-    case 'back':
-      bot.setControlState('back', true)
-      break
-    case 'left':
-      bot.setControlState('left', true)
-      break
-    case 'right':
-      bot.setControlState('right', true)
-      break
-    case 'sprint':
-      bot.setControlState('sprint', true)
-      break
-    case 'stop':
-      bot.clearControlStates()
-      break
-    case 'jump':
-      bot.setControlState('jump', true)
-      bot.setControlState('jump', false)
-      break
-    case 'jump a lot':
-      bot.setControlState('jump', true)
-      break
-    case 'stop jumping':
-      bot.setControlState('jump', false)
-      break
-    case 'attack':
-      entity = bot.nearestEntity()
-      if (entity) {
-        bot.attack(entity, true)
-      } else {
-        bot.chat('no nearby entities')
-      }
-      break
-    case 'mount':
-      entity = bot.nearestEntity((entity) => { return entity.type === 'object' })
-      if (entity) {
-        bot.mount(entity)
-      } else {
-        bot.chat('no nearby objects')
-      }
-      break
-    case 'dismount':
-      bot.dismount()
-      break
-    case 'move vehicle forward':
-      bot.moveVehicle(0.0, 1.0)
-      break
-    case 'move vehicle backward':
-      bot.moveVehicle(0.0, -1.0)
-      break
-    case 'move vehicle left':
-      bot.moveVehicle(1.0, 0.0)
-      break
-    case 'move vehicle right':
-      bot.moveVehicle(-1.0, 0.0)
-      break
-    case 'tp':
-      bot.entity.position.y += 10
-      break
-    case 'pos':
-      bot.chat(bot.entity.position.toString())
-      break
-    case 'yp':
-      bot.chat(`Yaw ${bot.entity.yaw}, pitch: ${bot.entity.pitch}`)
-      break
+async function commandHandler () {
+  for await (const [username, message] of on(bot, 'chat')) {
+    if (username === bot.username) return
+    target = bot.players[username].entity
+    let entity
+    switch (message) {
+      case 'forward':
+        bot.setControlState('forward', true)
+        break
+      case 'back':
+        bot.setControlState('back', true)
+        break
+      case 'left':
+        bot.setControlState('left', true)
+        break
+      case 'right':
+        bot.setControlState('right', true)
+        break
+      case 'sprint':
+        bot.setControlState('sprint', true)
+        break
+      case 'stop':
+        bot.clearControlStates()
+        break
+      case 'jump':
+        bot.setControlState('jump', true)
+        bot.setControlState('jump', false)
+        break
+      case 'jump a lot':
+        bot.setControlState('jump', true)
+        break
+      case 'stop jumping':
+        bot.setControlState('jump', false)
+        break
+      case 'attack':
+        entity = bot.nearestEntity()
+        if (entity) {
+          bot.attack(entity, true)
+        } else {
+          bot.chat('no nearby entities')
+        }
+        break
+      case 'mount':
+        entity = bot.nearestEntity((entity) => { return entity.type === 'object' })
+        if (entity) {
+          bot.mount(entity)
+        } else {
+          bot.chat('no nearby objects')
+        }
+        break
+      case 'dismount':
+        bot.dismount()
+        break
+      case 'move vehicle forward':
+        bot.moveVehicle(0.0, 1.0)
+        break
+      case 'move vehicle backward':
+        bot.moveVehicle(0.0, -1.0)
+        break
+      case 'move vehicle left':
+        bot.moveVehicle(1.0, 0.0)
+        break
+      case 'move vehicle right':
+        bot.moveVehicle(-1.0, 0.0)
+        break
+      case 'tp':
+        bot.entity.position.y += 10
+        break
+      case 'pos':
+        bot.chat(bot.entity.position.toString())
+        break
+      case 'yp':
+        bot.chat(`Yaw ${bot.entity.yaw}, pitch: ${bot.entity.pitch}`)
+        break
+    }
   }
-})
+}
 
-bot.once('spawn', () => {
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function looker () {
+  await once(bot, 'spawn')
   // keep your eyes on the target, so creepy!
-  setInterval(watchTarget, 50)
-
-  function watchTarget () {
-    if (!target) return
-    bot.lookAt(target.position.offset(0, target.height, 0))
+  while (true) {
+    await sleep(50)
+    if (!target) continue
+    await bot.lookAt(target.position.offset(0, target.height, 0))
   }
-})
+}
 
-bot.on('mount', () => {
-  bot.chat(`mounted ${bot.vehicle.objectType}`)
-})
+async function mounter () {
+  for await (const e of on(bot, 'mount')) { // eslint-disable-line
+    bot.chat(`mounted ${bot.vehicle.objectType}`)
+  }
+}
 
-bot.on('dismount', (vehicle) => {
-  bot.chat(`dismounted ${vehicle.objectType}`)
-})
+async function dismounter () {
+  for await (const [vehicle] of on(bot, 'dismount')) {
+    bot.chat(`dismounted ${vehicle.objectType}`)
+  }
+}
+
+commandHandler()
+looker()
+mounter()
+dismounter()
