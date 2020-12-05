6e9ac9314fb2eaa326f08d5e427703dc939d6fe3
Romain Beaumont <romain.rom1@gmail.com>
2020-12-05 21:37:09 +0100

    looker

--- examples/looker.js
+++ examples/looker.js
@@ -3,6 +3,7 @@
  * It checks for a near entity every tick.
  */
 const mineflayer = require('mineflayer')
+const { once } = require('events')
 
 if (process.argv.length < 4 || process.argv.length > 6) {
   console.log('Usage : node echo.js <host> <port> [<name>] [<password>]')
@@ -16,15 +17,23 @@ const bot = mineflayer.createBot({
   password: process.argv[5]
 })
 
-bot.once('spawn', function () {
-  setInterval(() => {
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function looker () {
+  await once(bot, 'spawn')
+  while (true) {
     const entity = bot.nearestEntity()
     if (entity !== null) {
       if (entity.type === 'player') {
-        bot.lookAt(entity.position.offset(0, 1.6, 0))
+        await bot.lookAt(entity.position.offset(0, 1.6, 0))
       } else if (entity.type === 'mob') {
-        bot.lookAt(entity.position)
+        await bot.lookAt(entity.position)
       }
     }
-  }, 50)
-})
+    await sleep(50)
+  }
+}
+
+looker()
