7a10cc3fc6b4be1c275800511370d62247b38f61
u9g <43508353+u9g@users.noreply.github.com>
2021-01-24 22:08:21 -0500

    added a ton more tests
    only inventory example not included is use

--- test/externalTests/exampleInventory.js
+++ test/externalTests/exampleInventory.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const tests = [
   {
     command: 'list',
-    wantedMessage: 'dirt x 64'
+    wantedMessage: 'dirt x 64, stone x 64, iron_ore x 64, diamond_boots x 1'
   },
 
   {
@@ -14,6 +14,41 @@ const tests = [
   {
     command: 'toss 64 dirt',
     wantedMessage: 'tossed 64 x dirt'
+  },
+
+  {
+    command: 'craft 16 stone_brick_stairs',
+    wantedMessage: 'I cannot make stone_brick_stairs'
+  },
+
+  {
+    command: 'craft 16 stone_bricks',
+    wantedMessage: 'did the recipe for stone_bricks 16 times'
+  },
+
+  {
+    command: 'craft 10 stone_brick_stairs',
+    wantedMessage: 'did the recipe for stick 10 stone_brick_stairs'
+  },
+
+  {
+    command: 'equip feet diamond_boots',
+    wantedMessage: 'equipped diamond_boots'
+  },
+
+  {
+    command: 'toss iron_ore',
+    wantedMessage: 'tossed iron_ore'
+  },
+
+  {
+    command: 'unequip feet',
+    wantedMessage: 'unequipped'
+  },
+
+  { // after tests layout
+    command: 'list',
+    wantedMessage: 'stone_bricks x 4, stone_brick_stairs x 40, diamond_boots x 1'
   }
 ]
 
@@ -21,7 +56,11 @@ module.exports = () => (bot, done) => {
   bot.test.runExample('examples/inventory.js', (ign, cb) => {
     assert.strictEqual(ign, 'inventory')
     bot.chat('/op inventory') // to counteract spawn protection
+    bot.chat('/setblock ~2 ~ ~2 crafting_table') // to make stone bricks stairs
     bot.chat('/give inventory dirt 64')
+    bot.chat('/give inventory stone 64')
+    bot.chat('/give inventory iron_ore 64')
+    bot.chat('/give inventory diamond_boots 1')
     const inventoryTest = Object.values(tests).map(testData => generateTest(testData, cb))
     // start test
     setTimeout(() => {
