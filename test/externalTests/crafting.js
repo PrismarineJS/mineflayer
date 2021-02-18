185d60fe4a065a40fddc1309b08b5f59ce3e594c
imharvol <imharvol@gmail.com>
2021-02-18 21:48:33 +0100

    Check the ammount of crafted items

--- test/externalTests/crafting.js
+++ test/externalTests/crafting.js
@@ -1,52 +1,28 @@
-const { Vec3 } = require('vec3')
+const assert = require('assert')
+const { once } = require('events')
 
 module.exports = () => async (bot) => {
-  const { blocksByName, itemsByName, findItemOrBlockByName } = require('minecraft-data')(bot.version)
   const Item = require('prismarine-item')(bot.version)
+  const { blocksByName, findItemOrBlockByName } = require('minecraft-data')(bot.version)
 
-  let populateBlockInventory
-  let craftItem
-  if (bot.supportFeature('oneBlockForSeveralVariations')) {
-    populateBlockInventory = blocksByName.log
-    craftItem = 'planks'
-  } else if (bot.supportFeature('blockSchemeIsFlat')) {
-    populateBlockInventory = itemsByName.birch_log
-    craftItem = 'birch_planks'
-  }
+  const craftingTableId = blocksByName.crafting_table.id
+  const goldIngotId = findItemOrBlockByName('gold_ingot').id
+  const goldBlockId = findItemOrBlockByName('gold_block').id
 
-  function findCraftingTable () {
-    const cursor = new Vec3(0, 0, 0)
-    for (cursor.x = bot.entity.position.x - 4; cursor.x < bot.entity.position.x + 4; cursor.x++) {
-      for (cursor.y = bot.entity.position.y - 4; cursor.y < bot.entity.position.y + 4; cursor.y++) {
-        for (cursor.z = bot.entity.position.z - 4; cursor.z < bot.entity.position.z + 4; cursor.z++) {
-          const block = bot.blockAt(cursor)
-          if (block.type === blocksByName.crafting_table.id) return block
-        }
-      }
-    }
-  }
+  // Place the crafting table
+  const craftingTablePosition = bot.entity.position.offset(2, 0, 0).floored()
+  bot.chat(`/setblock ${craftingTablePosition.toArray().join(' ')} crafting_table`)
+  await once(bot, `blockUpdate:${craftingTablePosition}`) // bot.test.wait(500)
+  const craftingTable = bot.blockAt(craftingTablePosition)
+  assert.strictEqual(craftingTable.type, craftingTableId, 'The crafting table was not placed correctly')
 
-  async function craft (amount, name) {
-    const item = findItemOrBlockByName(name)
-    const craftingTable = findCraftingTable()
-    const wbText = craftingTable ? 'with a crafting table, ' : 'without a crafting table, '
-    if (item == null) {
-      bot.test.sayEverywhere(`${wbText}unknown item: ${name}`)
-      throw new Error(`${wbText}unknown item: ${name}`)
-    } else {
-      const recipes = bot.recipesFor(item.id, null, 1, craftingTable) // doesn't check if it's possible to do it amount times
-      if (recipes.length) {
-        bot.test.sayEverywhere(`${wbText}I can make ${item.name}`)
-        await bot.craft(recipes[0], amount, craftingTable)
-        bot.test.sayEverywhere(`did the recipe for ${item.name} ${amount} times`)
-      } else {
-        bot.test.sayEverywhere(`${wbText}I can't make ${item.name}`)
-        throw new Error(`${wbText}I can't make ${item.name}`)
-      }
-    }
-  }
+  await bot.test.setInventorySlot(36, new Item(goldIngotId, 64, null, null))
 
-  await bot.test.setInventorySlot(36, new Item(populateBlockInventory.id, 1, 0))
-  await bot.test.becomeSurvival()
-  await craft(1, craftItem)
+  const goldBlocksRecipe = bot.recipesAll(goldBlockId, null, craftingTable)[0]
+  const craftCount = Math.floor(bot.inventory.count(goldIngotId) / 9)
+
+  console.log(`Crafting ${craftCount} golden blocks`)
+  await bot.craft(goldBlocksRecipe, craftCount, craftingTable)
+
+  assert.strictEqual(bot.inventory.count(goldBlockId), craftCount, 'It did not craft the correct amount')
 }
