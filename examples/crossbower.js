21c7e59399761f7ed48d0ed4e26c589486d067b0
sefirosweb <sefirosweb@gmail.com>
2021-02-28 16:45:50 +0100

    Improved example with quick_charge enchant

--- examples/crossbower.js
+++ examples/crossbower.js
@@ -13,19 +13,50 @@ const bot = mineflayer.createBot({
   password: process.argv[5]
 })
 
+bot.on('spawn', function () {
+  bot.chat(`/give ${bot.username} crossbow{Enchantments:[{id:quick_charge,lvl:3},{id:unbreaking,lvl:3}]} 1`) // Test with fast charge
+  // bot.chat(`/give ${bot.username} crossbow 1`) // Test with slow charge
+  bot.chat(`/give ${bot.username} minecraft:arrow 64`)
+})
+
 bot.on('chat', async (username, message) => {
   if (message === 'fire') {
-    if (bot.heldItem.nbt.value.Charged.value !== 1) { // already charged
-      bot.activateItem() // charge
-      await bot.waitForTicks(25) // wait for crossbow to charge, number from minecraft wiki
-      bot.deactivateItem() // raise weapon
+
+    // Check if weapon is equipped
+    const slotID = bot.getEquipmentDestSlot('hand')
+    if (bot.inventory.slots[slotID] === null || bot.inventory.slots[slotID].name !== 'crossbow') {
+      const weaponFound = bot.inventory.items().find(item => item.name === 'crossbow')
+      if (weaponFound) {
+        await bot.equip(weaponFound, 'hand')
+      } else {
+        console.log('No weapon in inventory')
+        stop()
+        return
+      }
     }
+
+    const isEnchanted = bot.heldItem.nbt.value.Enchantments ? bot.heldItem.nbt.value.Enchantments.value.value.find(enchant => {
+      return enchant.id.value === 'quick_charge'
+    }) : undefined
+
+    const timeForCharge = 1250 - ((isEnchanted ? isEnchanted.lvl.value : 0) * 250)
+
+    bot.activateItem() // charge
+    await sleep(timeForCharge) // wait for crossbow to charge
+    bot.deactivateItem() // raise weapon
+
     try {
       bot.lookAt(bot.players[username].entity.position, true)
       await bot.waitForTicks(5) // wait for lookat to finish
       bot.activateItem() // fire
+      bot.deactivateItem()
     } catch (err) {
       bot.chat('Player disappeared, crossbow is charged now.')
     }
   }
 })
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
