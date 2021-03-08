35f1c51f5a65cf371eccc7453806917d21be24e0
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2021-03-07 20:18:55 -0500

    Careated an example afk plugin
    added a plugin directory in the examples directory  
    Created an example plugin 
    
    close #1401

---
+++ examples/plugins/afk.js
@@ -0,0 +1,32 @@
+function inject(bot, option) {
+    
+    //it is a good idea to make a same space for your plugin
+    bot.afk = {
+        start,
+        stop,
+    }
+
+    let afk, rotation = 0
+
+    async function start() {
+
+        afk = setInterval(async ()=>{
+            if(rotation===0) {
+                await bot.look(0,0)
+                rotation=1
+            } else {
+                await bot.look(Math.PI,0)
+                rotation=0
+            }
+        },3000)
+    }
+
+    function stop() {
+        if (!afk) return 
+        clearInterval(afk)
+    }
+}
+
+module.exports = {
+    afk:inject
+}
