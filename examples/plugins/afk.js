642cc0002d1378fc8455352d69385056677cb315
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2021-03-07 20:27:08 -0500

    fixes the standard js issue
    pls pass the check

--- examples/plugins/afk.js
+++ examples/plugins/afk.js
@@ -1,32 +1,30 @@
-function inject(bot, option) {
-    
-    //it is a good idea to make a same space for your plugin
-    bot.afk = {
-        start,
-        stop,
-    }
+function inject (bot, option) {
+  // it is a good idea to make a same space for your plugin
+  bot.afk = {
+    start,
+    stop
+  }
 
-    let afk, rotation = 0
+  let afk; let rotation = 0
 
-    async function start() {
+  async function start () {
+    afk = setInterval(async () => {
+      if (rotation === 0) {
+        await bot.look(0, 0)
+        rotation = 1
+      } else {
+        await bot.look(Math.PI, 0)
+        rotation = 0
+      }
+    }, 3000)
+  }
 
-        afk = setInterval(async ()=>{
-            if(rotation===0) {
-                await bot.look(0,0)
-                rotation=1
-            } else {
-                await bot.look(Math.PI,0)
-                rotation=0
-            }
-        },3000)
-    }
-
-    function stop() {
-        if (!afk) return 
-        clearInterval(afk)
-    }
+  function stop () {
+    if (!afk) return
+    clearInterval(afk)
+  }
 }
 
 module.exports = {
-    afk:inject
+  afk: inject
 }
