3bafe79c59bc48a15934a2eeb567776d00d5c71c
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2021-03-22 11:27:59 -0400

    Added comments and fixed all the irregularities
    Closes #1742 by resolving all the comments

--- examples/plugins/afk.js
+++ examples/plugins/afk.js
@@ -1,30 +1,51 @@
+// Plugins are loaded using the bot.loadPlugin() function
+// The load plugin function excepts one function as its only parameter
+// The plugin runs the function passing it two parameter
+// The former is the bot object and the latter is the bot option
+// the options contain version player info and other plugins
+// Here is an example AFK plugin
+
+// Each plugin should have a inject function
+// This is the function that will be put in bot.loadPlugin
 function inject (bot, option) {
-  // it is a good idea to make a same space for your plugin
-  bot.afk = {
-    start,
-    stop
-  }
 
-  let afk; let rotation = 0
+// It is a good idea to make a namespace for your plugin
+  bot.afk = {}
 
-  async function start () {
-    afk = setInterval(async () => {
+  let afkInterval, rotation
+  // This namespace can be used to store some variuabels for user reffrence.
+  bot.afk.status = 'Idel'
+
+  // All your plugin functions should be in the name space other than the logic.
+  bot.afk.start = async function () {
+    afkInterval = setInterval(async () => {
       if (rotation === 0) {
         await bot.look(0, 0)
-        rotation = 1
+        rotation = true
       } else {
         await bot.look(Math.PI, 0)
-        rotation = 0
+        rotation = false
       }
     }, 3000)
+    bot.afk.status = 'afk'
   }
 
-  function stop () {
-    if (!afk) return
-    clearInterval(afk)
+  bot.afk.stop = function () {
+    if (afkInterval) {
+      clearInterval(afkInterval)
+      bot.afk.status = 'Idel'
+    }
   }
 }
 
+// Most of the time plugins are stored in different files so you must export them.
 module.exports = {
+  // Here you should name your inject function what ever you want.
   afk: inject
 }
+
+// This is a simple plugin with two methods, start and stop.
+// On the start method this plugin makes the bot look from east to west in 3-second intervals,
+// On stop it stops the bot from looking constantly spinning around
+// Additionally it also has a status variable inside the name space,
+// Users can use the variable as a reference for the code.
