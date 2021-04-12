5d540e1ca6c3803208a8cca55271d8e78ef2b564
DrMoraschi <68395930+DrMoraschi@users.noreply.github.com>
2021-04-12 07:36:18 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -63,7 +63,7 @@ interface BotEvents {
   actionBar: (jsonMsg: ChatMessage) => void;
   error: (err: Error) => void;
   message: (jsonMsg: ChatMessage, position: string) => void;
-  messagestr: (message: string, position: string) => void;
+  messagestr: (message: string, position: string, jsonMsg: ChatMessage) => void;
   unmatchedMessage: (stringMsg: string, jsonMsg: ChatMessage) => void;
   login: () => void;
   spawn: () => void;
