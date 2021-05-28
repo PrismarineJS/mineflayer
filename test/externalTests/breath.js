c37bbbbba3eb5a0929b0a1ab4a9044c66ce3c783
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-05-28 14:53:23 +0300

    Update breath.js

--- test/externalTests/breath.js
+++ test/externalTests/breath.js
@@ -1,7 +1,11 @@
 const assert = require('assert')
 const { once } = require('events')
+const { Vec3 } = require('vec3')
 
 module.exports = () => async (bot) => {
+  const mcData = require('minecraft-data')(bot.version)
+  const Item = require('prismarine-item')(bot.version)
+  
   await bot.test.setInventorySlot(36, new Item(mcData[blockItemsByName].water_bucket.id, 3, 0))
   await bot.test.becomeSurvival()
   await bot.test.placeBlock(36, new Vec3(0, 0, 1))
