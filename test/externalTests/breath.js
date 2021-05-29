64b6198dfdf5874019c0e326600ad90b229d7632
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-05-29 15:10:26 +0300

    Update breath.js

--- test/externalTests/breath.js
+++ test/externalTests/breath.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const { once } = require('events')
 
 module.exports = () => async (bot) => {
-  bot.chat('/setblock ~ ~1 ~ water')
+  bot.chat('/setblock ~ ~2 ~ water')
   await once(bot, 'breath')
   await new Promise((resolve, reject) => setTimeout(resolve, 5000))
   assert.strictEqual(bot.oxygenLevel, 20, 'Wrong oxygen level')
