9917c22fe8b0793a7cbb5095dab9b9701e43c990
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-06-01 23:25:50 +0300

    Update rayTrace.js

--- test/externalTests/rayTrace.js
+++ test/externalTests/rayTrace.js
@@ -8,19 +8,12 @@ module.exports = () => async (bot) => {
   let block = bot.blockAtCursor()
   assert.strictEqual(block, null)
 
-  block = bot.blockInSight()
-  assert.strictEqual(block, undefined)
-
   await bot.lookAt(position.offset(0, -3, 0), true)
 
   block = bot.blockAtCursor()
   const relBlock = bot.blockAt(position.offset(0, -1, 0))
   relBlock.face = BlockFace.TOP
 
-  assert.deepStrictEqual(block.position, relBlock.position)
-  assert.deepStrictEqual(block.face, relBlock.face)
-
-  block = bot.blockInSight()
   assert.deepStrictEqual(block.position, relBlock.position)
   assert.deepStrictEqual(block.face, relBlock.face)
 }
