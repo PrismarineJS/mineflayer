78c45b2a039dc150e090e71a1fc1957aac90a0e6
u9g <43508353+u9g@users.noreply.github.com>
2021-06-02 16:11:58 +0000

    Add another row of water

--- test/externalTests/placeEntity.js
+++ test/externalTests/placeEntity.js
@@ -39,11 +39,17 @@ module.exports = (version) => {
   })
 
   addTest('place boat', async (bot) => {
-    // far 3 blocks
+    /* Block Placement (o = water)
+      ooo
+      obo (b = water where we place the boat)
+      ooo
+    */
+    bot.chat('/setblock ~-1 ~-1 ~-3 water')
+    bot.chat('/setblock ~ ~-1 ~-3 water')
+    bot.chat('/setblock ~1 ~-1 ~-3 water')
     bot.chat('/setblock ~-1 ~-1 ~-2 water')
     bot.chat('/setblock ~ ~-1 ~-2 water')
     bot.chat('/setblock ~1 ~-1 ~-2 water')
-    // close 3 blocks
     bot.chat('/setblock ~-1 ~-1 ~-1 water')
     bot.chat('/setblock ~ ~-1 ~-1 water')
     bot.chat('/setblock ~1 ~-1 ~-1 water')
@@ -53,7 +59,6 @@ module.exports = (version) => {
     await p // await getting the boat
     const boat = await bot.placeEntity(bot.blockAt(bot.entity.position.offset(0, -1, -2)), new Vec3(0, 1, 0))
     assert(boat !== null)
-
     const entity = bot.nearestEntity(o => o.name === 'Boat')
     assert(entity?.name === 'Boat')
   })
