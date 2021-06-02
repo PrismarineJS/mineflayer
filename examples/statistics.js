7540ff96553de4f03751e15762a96b97c5e9b184
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-06-02 20:19:23 +0300

    Update statistics.js

--- examples/statistics.js
+++ examples/statistics.js
@@ -1,8 +1,20 @@
+/*
+ *
+ * A simple bot that requests statistics.
+ *
+ */
 const mineflayer = require('mineflayer')
 
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node statistics.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
 const bot = mineflayer.createBot({
-  username: 'KaffinPX',
-  version: '1.12.2'
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'stats',
+  password: process.argv[5]
 })
 
 bot.on('spawn', () => {
