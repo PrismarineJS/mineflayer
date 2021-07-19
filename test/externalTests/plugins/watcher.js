2c3cc9b8a1cae0be0df0977e578f21d9dfa244f8
u9g <43508353+u9g@users.noreply.github.com>
2021-07-19 17:30:18 -0400

    tab in

--- test/externalTests/plugins/watcher.js
+++ test/externalTests/plugins/watcher.js
@@ -1,26 +1,27 @@
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').headless
+module.exports = () => {
+  global.THREE = require('three')
+  global.Worker = require('worker_threads').Worker
+  const { createCanvas, ImageData } = require('node-canvas-webgl/lib')
 
-global.THREE = require('three')
-global.Worker = require('worker_threads').Worker
-const { createCanvas, ImageData } = require('node-canvas-webgl/lib')
+  // Patch global scope to imitate browser environment.
+  global.window = global
+  global.ImageData = ImageData
+  global.document = {
+    createElement: (nodeName) => {
+      if (nodeName !== 'canvas') throw new Error(`Cannot create node ${nodeName}`)
+      const canvas = createCanvas(256, 256)
+      return canvas
+    },
+    type: 'synthetic'
+  }
 
-// Patch global scope to imitate browser environment.
-global.window = global
-global.ImageData = ImageData
-global.document = {
-  createElement: (nodeName) => {
-    if (nodeName !== 'canvas') throw new Error(`Cannot create node ${nodeName}`)
-    const canvas = createCanvas(256, 256)
-    return canvas
-  },
-  type: 'synthetic'
+  const bot = mineflayer.createBot({
+    username: 'watcher_bot'
+  })
+
+  bot.once('spawn', () => {
+    mineflayerViewer(bot, { output: 'output.mp4', frames: -1, width: 512, height: 512 })
+  })
 }
-
-const bot = mineflayer.createBot({
-  username: 'watcher_bot'
-})
-
-bot.once('spawn', () => {
-  mineflayerViewer(bot, { output: 'output.mp4', frames: -1, width: 512, height: 512 })
-})
