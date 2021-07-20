5045c996544c0867f323f3d9c8ee2d57f3b09798
BlueBurgersTDD <58081413+TheBlueBurger@users.noreply.github.com>
2021-07-20 11:48:29 +0200

    last linting issue i hope

--- index.d.ts
+++ index.d.ts
@@ -27,7 +27,7 @@ export interface BotOptions extends ClientOptions {
 }
 
 // There has to be a better way to do this
-export type supportedVersionsForBotOptions = 
+export type supportedVersionsForBotOptions =
 '1.8' | '1.8.1'| '1.8.2' | '1.8.3' | '1.8.4' | '1.8.5' | '1.8.6' | '1.8.7' | '1.8.8' | '1.8.9'
 | '1.9' | '1.9.1' | '1.9.2' | '1.9.3' | '1.9.4'
 | '1.10' | '1.10.1' | '1.10.2'
