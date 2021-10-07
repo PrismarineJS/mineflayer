2673271ee9811c7b8bdcbe4de3d87ed0b609f8a5
usb <56985400+U5B@users.noreply.github.com>
2021-10-07 10:43:30 -0700

    add typing

--- index.d.ts
+++ index.d.ts
@@ -26,6 +26,7 @@ export interface BotOptions extends ClientOptions {
   physicsEnabled?: boolean
   client?: Client
   brand?: string
+  defaultChatPatterns: boolean
 }
 
 export type ChatLevel = 'enabled' | 'commandsOnly' | 'disabled'
