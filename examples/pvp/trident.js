092893f78ff71c124ed1d7eaaea92eb0ce7c193f
Firejoust <42525540+firejoust@users.noreply.github.com>
2021-12-09 00:32:15 +0000

    blend in trident.js with other examples

--- examples/pvp/trident.js
+++ examples/pvp/trident.js
@@ -1,6 +1,17 @@
 const mineflayer = require('mineflayer')
 const projectile = require('mineflayer-projectile')
-const bot = mineflayer.createBot() // etc. (https://github.com/PrismarineJS/mineflayer/blob/master/docs/api.md#mineflayercreatebotoptions)
+
+if (process.argv.length < 4 || process.argv.length > 6) {
+  console.log('Usage : node trident.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'anvilman',
+  password: process.argv[5]
+})
 
 bot.once('login', login)
 
