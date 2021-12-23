1ab7cd5bb55cbf0b317da0f0b0ed75634164201b
IceTank <niklaslehle@gmail.com>
2021-12-23 18:40:29 +0100

    Add waitForTaskCompatibility Renmaed createTask Code refactor Fixed digging not working Fixed broken fishing test Cancel previous looking task when calling look

--- test/externalTests/fishing.js
+++ test/externalTests/fishing.js
@@ -4,16 +4,20 @@ module.exports = () => async (bot) => {
   bot.test.sayEverywhere('/fill ~-5 ~-1 ~-5 ~5 ~-1 ~5 water')
   await bot.test.setInventorySlot(36, new Item(bot.registry.itemsByName.fishing_rod.id, 1, 0))
   await bot.lookAt(bot.entity.position) // dont force the position
-  bot.fish()
 
-  return new Promise((resolve, reject) => {
-    function onPlayerCollect (collector, collected) {
-      if (collected.type === 'object') {
-        bot.test.sayEverywhere('I caught: ' + collected.displayName)
-        bot.removeListener('playerCollect', onPlayerCollect)
-        resolve()
+  const waitPlayerCollect = () => {
+    return new Promise((resolve, reject) => {
+      function onPlayerCollect (collector, collected) {
+        if (collected.type === 'object') {
+          bot.test.sayEverywhere('I caught: ' + collected.displayName)
+          bot.removeListener('playerCollect', onPlayerCollect)
+          resolve()
+        }
       }
-    }
-    bot.on('playerCollect', onPlayerCollect)
-  })
+      
+      bot.on('playerCollect', onPlayerCollect)
+    })
+  }
+
+  await Promise.allSettled([bot.fish(), waitPlayerCollect()])
 }
