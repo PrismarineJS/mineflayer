165e0e17fd3a308e1844ba98b0e8dad536f3c4f2
Romain Beaumont <romain.rom1@gmail.com>
2022-01-10 20:56:17 +0100

    Try to wait a bit longer in find block test

--- test/externalTests/exampleBlockFinder.js
+++ test/externalTests/exampleBlockFinder.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 module.exports = () => async (bot) => {
   await bot.test.runExample('examples/blockfinder.js', async (name, cb) => {
     assert.strictEqual(name, 'finder')
-    await bot.test.wait(2000)
+    await bot.test.wait(8000)
     await bot.test.tellAndListen(name, 'find dirt', (message) => {
       const matches = message.match(/I found ([0-9]+) (.+?) blocks in (.+?) ms/)
       if (matches.length !== 4 || matches[1] === '0' || matches[2] !== 'dirt' || parseFloat(matches[3]) > 500) {
