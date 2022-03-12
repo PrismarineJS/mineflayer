f171295a9ec09c6860437b26a450c93e74aee1d7
u9g <git@u9g.dev>
2022-03-12 17:01:52 -0500

    Add bot.creative.clearSlot and bot.creative.clearInventory

--- test/externalTests/creative.js
+++ test/externalTests/creative.js
@@ -25,4 +25,16 @@ module.exports = () => async (bot) => {
   assert.strictEqual(bot.inventory.slots[SLOT].type, 4)
   await bot.creative.setInventorySlot(SLOT, null)
   assert.strictEqual(bot.inventory.slots[SLOT], null)
+  // clear slot
+  await bot.creative.setInventorySlot(SLOT, new Item(4, 1, 0))
+  assert.strictEqual(bot.inventory.slots[SLOT].type, 4)
+  await bot.creative.clearSlot(SLOT)
+  assert.strictEqual(bot.inventory.slots[SLOT], null)
+  // clear inventory
+  for (let i = 0; i < 9; i++) {
+    await bot.creative.setInventorySlot(36 + i, new Item(1, 1, 0))
+  }
+  assert.strictEqual(bot.inventory.slots.filter(item => item).length, 9)
+  await bot.creative.clearInventory()
+  assert.strictEqual(bot.inventory.slots.filter(item => item).length, 0)
 }
