35c056d6b362c23cb2bcb0e72e4d95340defd2de
Citycrafty <105089147+Citycrafty@users.noreply.github.com>
2022-06-10 10:40:23 +0530

    Update gps.js

--- examples/pathfinder/gps.js
+++ examples/pathfinder/gps.js
@@ -8,10 +8,11 @@ if (process.argv.length < 4 || process.argv.length > 6) {
   process.exit(1)
 }
 
+
 const bot = mineflayer.createBot({
-  host: process.argv[2],
-  port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'gps',
+  host: City_crafty.aternos.me,
+  port: 62930),
+  username:'gps',
   password: process.argv[5]
 })
 
