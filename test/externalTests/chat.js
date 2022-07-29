1bfc3dd1a8b36bb83fa57ed1b583d409db3ef11b
QimieGames <45062664+QimieGames@users.noreply.github.com>
2022-07-30 03:32:25 +0800

    Updated tests.

--- test/externalTests/chat.js
+++ test/externalTests/chat.js
@@ -74,19 +74,20 @@ module.exports = () => {
   })
 
   addTest('test awaitMessage', async (bot) => {
-    // let resolves = 0
-    const p1 = bot.awaitMessage(5000, '<flatbot> hello').then(msg => console.log(msg)).catch(err => console.log(err))
+    // tests if message is found in set time
+    bot.awaitMessage(5000, '<flatbot> hello').then(msg => console.log(msg)).catch(err => console.log(err))
     bot.chat('hello')
-    await p1
-    const p2 = bot.awaitMessage(5000, ['<flatbot> hello', '<flatbot> world']).then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, ['<flatbot> hello', '<flatbot> world']).then(msg => console.log(msg)).catch(err => console.log(err))
     bot.chat('world')
-    await p2
-    const p3 = bot.awaitMessage(5000, /<.+> hello/).then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, /<.+> hello/).then(msg => console.log(msg)).catch(err => console.log(err))
     bot.chat('hello')
-    await p3
-    const p4 = bot.awaitMessage(5000, [/<.+> hello/, /<.+> world/]).then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, [/<.+> hello/, /<.+> world/]).then(msg => console.log(msg)).catch(err => console.log(err))
     bot.chat('world')
-    await p4
+    // tests if message not found in set time
+    bot.awaitMessage(5000, '<flatbot> hello').then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, ['<flatbot> hello', '<flatbot> world']).then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, /<.+> hello/).then(msg => console.log(msg)).catch(err => console.log(err))
+    bot.awaitMessage(5000, [/<.+> hello/, /<.+> world/]).then(msg => console.log(msg)).catch(err => console.log(err))
   })
 
   addTest('test removechatpattern with a number input', async (bot) => {
