f185000962eccd73a0b3d3c7623d031d399bb01a
Nyxa <116373322+NyxaYu@users.noreply.github.com>
2022-10-25 21:40:28 +0100

    Linting fix

--- test/externalTests/particles.js
+++ test/externalTests/particles.js
@@ -1,10 +1,10 @@
 const assert = require('assert')
 
 module.exports = () => async (bot) => {
-  const particleData = bot.registry.particles[0];
+  const particleData = bot.registry.particles[0]
 
   return new Promise((resolve, reject) => {
-    function onParticleEvent(particle){
+    function onParticleEvent (particle) {
       assert.strictEqual(particle.id, particleData.id)
       assert.strictEqual(particle.name, particleData.name)
       assert.strictEqual(particle.position.x, bot.entity.position.x)
@@ -22,6 +22,6 @@ module.exports = () => async (bot) => {
 
     bot.on('particle', onParticleEvent)
 
-    bot.chat(`/particle ${particleData.name} ~ ~ ~ 5 5 5 0.5 100 force`);
+    bot.chat(`/particle ${particleData.name} ~ ~ ~ 5 5 5 0.5 100 force`)
   })
 }
