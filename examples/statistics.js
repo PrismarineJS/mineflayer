d6e74ddf2521333883a907b90d216604598c28ad
a <>
2023-06-08 18:14:54 -0400

    revamp testing

--- examples/statistics.js
+++ examples/statistics.js
@@ -4,6 +4,7 @@
  *
  */
 const mineflayer = require('mineflayer')
+const assert = require('assert')
 
 if (process.argv.length < 4 || process.argv.length > 6) {
   console.log('Usage : node statistics.js <host> <port> [<name>] [<password>]')
@@ -14,11 +15,28 @@ const bot = mineflayer.createBot({
   host: process.argv[2],
   port: parseInt(process.argv[3]),
   username: process.argv[4] ? process.argv[4] : 'stats',
-  password: process.argv[5]
+  password: process.argv[5],
+  verbose: true
 })
 
-bot.on('spawn', () => {
-  bot.requestStatistics().then((data) => {
-    console.log(data)
-  })
+bot.on('chat', async (username, message) => {
+  if (username === bot.username) return
+  switch (message) {
+    case 'loaded':
+      await bot.waitForChunksToLoad()
+      bot.chat('Ready!')
+      break
+    case 'test':
+      // Jump Statistic
+      bot.setControlState('jump', true)
+      await bot.waitForTicks(1)
+      bot.setControlState('jump', false)
+
+      // Death Statistic
+      bot.chat('/kill')
+
+      const statistics = await bot.requestStatistics()
+      bot.chat(`deaths=${statistics['stat.deaths']};timeLastDeath=${statistics['stat.timeSinceDeath']};jumps=${statistics['stat.jump']}`)
+      break
+  }
 })
--- test/externalTests/statistics.js
+++ test/externalTests/statistics.js
@@ -1,9 +1,28 @@
 const assert = require('assert')
 
 module.exports = () => async (bot) => {
+  bot.chat('/kill')
+
   const stats = await bot.requestStatistics()
 
   console.log(stats)
 
-  assert.strictEqual(typeof stats, 'object')
+  // Easiest Stats to Test, not dependent on any other tests
+  assert(stats['stat.deaths'] > 0)
+  assert(stats['stat.timeSinceDeath'] === 0)
+  assert(stats['stat.playOneMinute'] > 0)
+
+  // Stats that are dependent on previous tests
+  // assert(stats['stat.craftingTableInteraction'] === 1)
+  // assert(stats['stat.craftItem.minecraft.ladder'] === 3)
+  // assert(stats['stat.craftItem.minecraft.planks'] === 4)
+  // assert(stats['stat.itemEnchanted'] === 1)
+  // assert(stats['stat.jump'] === 1)
+  // assert(stats['stat.mineBlock.minecraft.dirt'] === 1)
+  // assert(stats['stat.pickup.minecraft.dirt'] === 1)
+  // assert(stats['stat.sleepInBed'] === 1)
+  // assert(stats['stat.useItem.minecraft.bookshelf'] === 15)
+  // assert(stats['stat.useItem.minecraft.bread'] === 4)
+  // assert(stats['stat.useItem.minecraft.dirt'] === 1)
+  // assert(stats['stat.useItem.minecraft.enchanting_table'] === 1)
 }
