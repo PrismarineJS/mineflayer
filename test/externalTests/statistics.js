bdf336c5461ad3a47f121f3917150c3a58bce5ca
a <>
2023-06-13 20:00:01 -0400

    update tests

--- test/externalTests/statistics.js
+++ test/externalTests/statistics.js
@@ -1,28 +1,20 @@
 const assert = require('assert')
 
 module.exports = () => async (bot) => {
-  bot.chat('/kill')
+  await bot.test.wait(1000) // Make sure everything is updated?
 
   const stats = await bot.requestStatistics()
 
   console.log(stats)
 
-  // Easiest Stats to Test, not dependent on any other tests
-  assert(stats['stat.deaths'] > 0)
-  assert(stats['stat.timeSinceDeath'] === 0)
-  assert(stats['stat.playOneMinute'] > 0)
+  // Easiest Stat to Test, not dependent on any other tests
+  assert(stats['minecraft.play_one_minute'] > 0)
 
-  // Stats that are dependent on previous tests
-  // assert(stats['stat.craftingTableInteraction'] === 1)
-  // assert(stats['stat.craftItem.minecraft.ladder'] === 3)
-  // assert(stats['stat.craftItem.minecraft.planks'] === 4)
-  // assert(stats['stat.itemEnchanted'] === 1)
-  // assert(stats['stat.jump'] === 1)
-  // assert(stats['stat.mineBlock.minecraft.dirt'] === 1)
-  // assert(stats['stat.pickup.minecraft.dirt'] === 1)
-  // assert(stats['stat.sleepInBed'] === 1)
-  // assert(stats['stat.useItem.minecraft.bookshelf'] === 15)
-  // assert(stats['stat.useItem.minecraft.bread'] === 4)
-  // assert(stats['stat.useItem.minecraft.dirt'] === 1)
-  // assert(stats['stat.useItem.minecraft.enchanting_table'] === 1)
+  // Dependent On Other Tests - Free To Remove If Errors
+  // Test: crafting
+  assert(stats['minecraft.crafted.minecraft.ladder'] >= 1)
+
+  // Test: digAndBuild
+  assert(stats['minecraft.mined.minecraft.dirt'] >= 1)
+  assert(stats['minecraft.used.minecraft.dirt'] >= 1)
 }
