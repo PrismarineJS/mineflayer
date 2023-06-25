e77d5b4ecacf4b18fabf62d7aaa7e448fd3c4900
PondWader <66561610+PondWader@users.noreply.github.com>
2023-06-25 19:56:09 +0100

    Revert node 18 + place block change

--- test/externalTests/commandBlock.js
+++ test/externalTests/commandBlock.js
@@ -3,17 +3,17 @@ const { once } = require('events')
 const { Vec3 } = require('vec3')
 
 module.exports = () => async (bot) => {
-  return new Promise(async (resolve, reject) => {
-    const command = `/say ${Math.floor(Math.random() * 1000)}`
-    const commandBlockPos = new Vec3(1, 5, 1)
-    const commandBlockPosText = commandBlockPos.toArray().join(' ')
-  
-    // Put and activate the command block
-    const p = once(bot.world, `blockUpdate:(${commandBlockPos.x}, ${commandBlockPos.y}, ${commandBlockPos.z})`)
-    bot.test.sayEverywhere(`/setblock ${commandBlockPosText} minecraft:command_block`)
-    await p
-    bot.setCommandBlock(commandBlockPos, command, false)
-  
+  const command = `/say ${Math.floor(Math.random() * 1000)}`
+  const commandBlockPos = new Vec3(1, 5, 1)
+  const commandBlockPosText = commandBlockPos.toArray().join(' ')
+
+  // Put and activate the command block
+  const p = once(bot.world, `blockUpdate:(${commandBlockPos.x}, ${commandBlockPos.y}, ${commandBlockPos.z})`)
+  bot.test.sayEverywhere(`/setblock ${commandBlockPosText} minecraft:command_block`)
+  await p
+  bot.setCommandBlock(commandBlockPos, command, false)
+
+  return new Promise((resolve, reject) => {
     const messageListener = (message) => {
       if (message.json.with[0] === command) {
         assert(message.json.translate === 'advMode.setCommand.success')
