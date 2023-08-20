d3df34dcaa804a71bf0d8cc50a419990d4a2dce3
extremeheat <extreme@protonmail.ch>
2023-08-20 13:40:23 -0400

    Use node 18 on CI (#3157)
    * Delete .gitpod.DockerFile
    
    * use node 16 on CI
    
    * replace localhost with 127.0.0.1
    
    * Update index.js
    
    16 to 18
    
    ---------
    
    Co-authored-by: Romain Beaumont <romain.rom1@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 18) {
   console.error('Your node version is currently', process.versions.node)
-  console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
+  console.error('Please update it to a version >= 18.x.x from https://nodejs.org/')
   process.exit(1)
 }
 
