1caa2c216b3a10a2bccd7b78a22f3809cb359fe3
Evan Goode <mail@evangoo.de>
2023-12-30 09:17:29 -0500

    Fix several bugs in villager trading (#3230)
    * Fix several bugs in villager trading
    
    These changes should fix issues with trades that have more than one item
    in the output stack, trades that cost more than one emerald, and trades
    with two input items, such as trades for enchanted books and tipped
    arrows.
    
    * Add more tests for villager trading
    
    - Add a test trade taking 1 emerald and returning 4 glass
    - Add a test trade taking [36 emeralds, 1 book] and returning 1 wooden sword
      (any unstackable should work to accurately simulate an enchanted_book
      trade)
    - Increase maximumNbTradeUses to 12 for all test trades
    
    ---------
    
    Co-authored-by: Romain Beaumont <romain.rom1@gmail.com>

--- test/externalTests/trade.js
+++ test/externalTests/trade.js
@@ -6,14 +6,20 @@ module.exports = () => async (bot) => {
 
   const villagerType = bot.registry.entitiesByName.villager ? 'villager' : 'Villager'
   const testFluctuations = bot.supportFeature('selectingTradeMovesItems')
+
   const summonCommand = bot.supportFeature('indexesVillagerRecipes')
-    ? `/summon ${villagerType} ~ ~1 ~ {NoAI:1, Offers:{Recipes:[0:{maxUses:7,buy:{id:"minecraft:emerald",Count:2},sell:{id:"minecraft:pumpkin_pie",Count:2},uses: 1},1:{maxUses:7,buy:{id:"minecraft:emerald",Count:2},buyB:{id:"minecraft:pumpkin_pie",Count:2},sell:{id:"minecraft:wheat",Count:2}, uses:1}]}}`
-    : `/summon ${villagerType} ~ ~1 ~ {NoAI:1, Offers:{Recipes:[{maxUses:7,buy:{id:"minecraft:emerald",Count:2},sell:{id:"minecraft:pumpkin_pie",Count:2},${testFluctuations ? 'demand:60,priceMultiplier:0.05f,specialPrice:-4,' : ''}uses: 1},{maxUses:7,buy:{id:"minecraft:emerald",Count:2},buyB:{id:"minecraft:pumpkin_pie",Count:2},sell:{id:"minecraft:wheat",Count:2}, uses:1}]}}`
+    ? `/summon ${villagerType} ~ ~1 ~ {NoAI:1, Offers:{Recipes:[0:{maxUses:12,buy:{id:"minecraft:emerald",Count:2},sell:{id:"minecraft:pumpkin_pie",Count:2},uses: 1},1:{maxUses:12,buy:{id:"minecraft:emerald",Count:2},buyB:{id:"minecraft:pumpkin_pie",Count:2},sell:{id:"minecraft:wheat",Count:2}, uses:1},2:{maxUses:12,buy:{id:"minecraft:emerald",Count:1},sell:{id:"minecraft:glass",Count:4},uses: 1},3:{maxUses:12,buy:{id:"minecraft:emerald",Count:36},buyB:{id:"minecraft:book",Count:1},sell:{id:"minecraft:wooden_sword",Count:1},uses: 1}]}}`
+    : `/summon ${villagerType} ~ ~1 ~ {NoAI:1, Offers:{Recipes:[{maxUses:12,buy:{id:"minecraft:emerald",Count:2},sell:{id:"minecraft:pumpkin_pie",Count:2},${testFluctuations ? 'demand:60,priceMultiplier:0.05f,specialPrice:-4,' : ''}uses: 1},{maxUses:12,buy:{id:"minecraft:emerald",Count:2},buyB:{id:"minecraft:pumpkin_pie",Count:2},sell:{id:"minecraft:wheat",Count:2}, uses:1},{maxUses:12,buy:{id:"minecraft:emerald",Count:1},sell:{id:"minecraft:glass",Count:4},uses: 1},{maxUses:12,buy:{id:"minecraft:emerald",Count:36},buyB:{id:"minecraft:book",Count:1},sell:{id:"minecraft:wooden_sword",Count:1},uses: 1}]}}`
 
   const commandBlockPos = bot.entity.position.offset(0.5, 0, 0.5)
   const redstoneBlockPos = commandBlockPos.offset(1, 0, 0)
 
-  await bot.test.setInventorySlot(36, new Item(bot.registry.itemsByName.emerald.id, 64, 0))
+  let shouldHaveEmeralds = 0
+  for (let slot = 9; slot <= 17; slot += 1) {
+    await bot.test.setInventorySlot(slot, new Item(bot.registry.itemsByName.emerald.id, 64, 0))
+    shouldHaveEmeralds += 64
+  }
+  await bot.test.setInventorySlot(18, new Item(bot.registry.itemsByName.book.id, 11, 0))
 
   // A command block is needed to spawn the villager due to the chat's character limit in some versions
   bot.test.sayEverywhere(`/setblock ${commandBlockPos.toArray().join(' ')} command_block`)
@@ -40,9 +46,10 @@ module.exports = () => async (bot) => {
     assert.strictEqual(output.name, 'pumpkin_pie')
     assert.strictEqual(output.count, 2)
 
-    await bot.trade(villager, 0, 6)
-    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), testFluctuations ? 64 - 24 : 64 - 12)
-    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.pumpkin_pie.id), 12)
+    await bot.trade(villager, 0, 11)
+    shouldHaveEmeralds -= testFluctuations ? (2 * 2 * 11) : (2 * 11)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), shouldHaveEmeralds)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.pumpkin_pie.id), 22)
   }
 
   // Handle trade #2 -- takes [2x emerald, 2x pumpkin_pie] and returns 2x wheat
@@ -61,15 +68,59 @@ module.exports = () => async (bot) => {
     assert.strictEqual(output.name, 'wheat')
     assert.strictEqual(output.count, 2)
 
-    await bot.trade(villager, 1, 6)
-    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), testFluctuations ? 64 - 36 : 64 - 24)
+    await bot.trade(villager, 1, 11)
+    shouldHaveEmeralds -= 11 * 2
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), shouldHaveEmeralds)
     assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.pumpkin_pie.id), 0)
-    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.wheat.id), 12)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.wheat.id), 22)
+  }
+
+  // Handle trade #3 -- takes 1x emerald and returns 4x glass
+  {
+    const trade = villager.trades[2]
+    assert.strictEqual(trade.inputs.length, 1, 'Expected single input from villager on first trade')
+    verifyTrade(trade)
+
+    const [input] = trade.inputs
+    assert.strictEqual(input.name, 'emerald')
+    assert.strictEqual(input.count, 1)
+
+    const [output] = trade.outputs
+    assert.strictEqual(output.name, 'glass')
+    assert.strictEqual(output.count, 4)
+
+    await bot.trade(villager, 2, 11)
+    shouldHaveEmeralds -= 11
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), shouldHaveEmeralds)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.glass.id), 44)
+  }
+
+  // Handle trade #4 -- takes [36x emerald, 1x book] and returns 1x wooden sword
+  {
+    const trade = villager.trades[3]
+    assert.strictEqual(trade.inputs.length, 2, 'Expected two inputs from villager on second trade')
+    verifyTrade(trade)
+
+    const [input1, input2] = trade.inputs
+    assert.strictEqual(input1.name, 'emerald')
+    assert.strictEqual(input1.count, 36)
+    assert.strictEqual(input2.name, 'book')
+    assert.strictEqual(input2.count, 1)
+
+    const [output] = trade.outputs
+    assert.strictEqual(output.name, 'wooden_sword')
+    assert.strictEqual(output.count, 1)
+
+    await bot.trade(villager, 3, 11)
+    shouldHaveEmeralds -= 11 * 36
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.emerald.id), shouldHaveEmeralds)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.book.id), 0)
+    assert.strictEqual(bot.currentWindow.count(bot.registry.itemsByName.wooden_sword.id), 11)
   }
 
   function verifyTrade (trade) {
     assert.strictEqual(trade.nbTradeUses, 1)
-    assert.strictEqual(trade.maximumNbTradeUses, 7)
+    assert.strictEqual(trade.maximumNbTradeUses, 12)
     assert.strictEqual(trade.tradeDisabled, false)
 
     const printCountInv = function (item) {
