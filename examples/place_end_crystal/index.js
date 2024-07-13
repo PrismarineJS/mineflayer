78b4eccb4572a821b11c3124b7a593f3b91f1180
AreaDenial <32404366+AreaDenial@users.noreply.github.com>
2024-07-13 17:06:39 -0400

    Player hitbox fixes (#3382)
    * Update entities.js
    
    * Update ray_trace.js
    
    Replace raytrace start position with bot.entity.eyeHeight
    Remove old fix for player hitbox height
    
    * Update digging.js
    
    Set raytrace start position to bot.entity.eyeHeight
    
    * Update index.js
    
    Minor typo in comment lol
    
    * Update physics.js
    
    Change lookAt start position to use bot.entity.eyeHeight
    
    * Update blocks.js
    
    Change canSeeBlock to use bot.entity.eyeHeight
    
    * Update entities.js
    
    Update player height on crouch/uncrouch
    
    * Update entities.js
    
    Initialize bot.entity.height and bot.entity.width on login
    
    * Update entities.js
    
    Typo
    
    * Update entities.js
    
    Initialize bot entity eyeHeight on login

--- examples/place_end_crystal/index.js
+++ examples/place_end_crystal/index.js
@@ -51,7 +51,7 @@ bot.on('chat', async (ign, msg) => {
       const { x: aboveX, y: aboveY, z: aboveZ } = block.position.offset(0, 1, 0)
       const blockBoundingBox = new AABB(aboveX, aboveY, aboveZ, aboveX + 1, aboveY + 2, aboveZ + 1)
       const entityAABBs = Object.values(bot.entities).map(entity => {
-        // taken from taken from https://github.com/PrismarineJS/prismarine-physics/blob/d145e54a4bb8604300258badd7563f59f2101922/index.js#L92
+        // taken from https://github.com/PrismarineJS/prismarine-physics/blob/d145e54a4bb8604300258badd7563f59f2101922/index.js#L92
         const w = entity.height / 2
         const { x, y, z } = entity.position
         return new AABB(-w, 0, -w, w, entity.height, w).offset(x, y, z)
