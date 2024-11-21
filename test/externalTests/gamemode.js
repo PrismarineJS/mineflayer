e346813ed2f34f0e0c1a9f9f86e5ccecbf433e69
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2024-11-21 19:42:00 +0100

    Fix gamemode test and implementation

--- test/externalTests/gamemode.js
+++ test/externalTests/gamemode.js
@@ -1,10 +1,30 @@
 // test to see if bot retains creative gamemode in bot object on death
 
 const assert = require('assert')
+const { onceWithCleanup } = require('../../lib/promise_utils')
 
-module.exports = async (bot) => {
-  bot.test.becomeCreative()
-  bot.chat(`/kill ${bot.username}`)
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000))
-  assert.strictEqual(bot.game.gameMode, 'creative', 'Failed to parse respawn packet')
+module.exports = () => {
+  const tests = []
+
+  function addTest (name, f) {
+    tests[name] = (bot) => f(bot)
+  }
+
+  addTest('change', async (bot) => {
+    await bot.test.becomeSurvival()
+    assert.strictEqual(bot.game.gameMode, 'survival', 'Wrong gamemode after switching gamemode')
+    await bot.test.becomeCreative()
+    assert.strictEqual(bot.game.gameMode, 'creative', 'Wrong gamemode after switching gamemode')
+  })
+
+  addTest('after respawn', async (bot) => {
+    await bot.test.becomeCreative()
+
+    bot.chat('/kill')
+
+    await onceWithCleanup(bot, 'respawn', { timeout: 2000 })
+    assert.strictEqual(bot.game.gameMode, 'creative', 'Wrong gamemode after respawn')
+  })
+
+  return tests
 }
