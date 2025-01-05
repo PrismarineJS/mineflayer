071918b84aae567b140ba2f59e5f346185179832
FreezeEngine <freezeengine@gmail.com>
2025-01-05 22:36:03 +0300

    New 'version' format, support for it in loader, example and CI

--- examples/bedrock/chat_ping_pong.js
+++ examples/bedrock/chat_ping_pong.js
@@ -3,21 +3,18 @@ const mineflayer = require('mineflayer')
 const username = 'Bot'
 const server = 'localhost'
 
-options = {
-    version: 'bedrock_1.21.50',
-    host: server,
-    skipPing: true,
-    viewDistance: 5,
-    autoInitPlayer: true,
-    port: 19132,
-    offline: true,
-    username: username
+const options = {
+  version: 'bedrock',
+  host: server,
+  port: 19132,
+  offline: true,
+  username
 }
 
 const bot = mineflayer.createBot(options)
 
 bot.on('message', (message, type, sender, verified) => {
-    if (sender === bot._client.username) return
-    console.log([message, type, sender, verified])
-    bot.chat(message.getText(0))
+  if (sender === bot._client.username) return
+  console.log([message, type, sender, verified])
+  bot.chat(message.getText(0))
 })
