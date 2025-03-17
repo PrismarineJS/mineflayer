1c7e139e45303047b51da3176c004640665a5283
Sinan <shuriken209master@googlemail.com>
2025-03-17 22:59:56 +0100

    Add test (hope this works)

---
+++ test/externalTests/switchWorldEvent.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const { once, onceWithCleanup } = require('../../lib/promise_utils')
+
+module.exports = () => async (bot) => {
+  await once(bot, 'spawn')
+
+  const switchWorldPromise = onceWithCleanup(bot, 'switchWorld', {
+    timeout: 15000
+  })
+
+  bot.test.sayEverywhere('/setblock ~ ~ ~ nether_portal')
+  bot.test.sayEverywhere('/setblock ~ ~ ~ portal')
+
+  await switchWorldPromise
+
+  console.log('Bot switched worlds, switchWorld event was emitted!')
+}
