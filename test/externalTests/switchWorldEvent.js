2e564efe09d3a4fba70007985a8765171a9e0476
Sinan <shuriken209master@googlemail.com>
2025-03-17 23:40:01 +0100

    test

--- test/externalTests/switchWorldEvent.js
+++ test/externalTests/switchWorldEvent.js
@@ -1,4 +1,5 @@
 const { once, onceWithCleanup } = require('../../lib/promise_utils')
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports = () => async (bot) => {
   await once(bot, 'spawn')
@@ -6,6 +7,24 @@ module.exports = () => async (bot) => {
   const switchWorldPromise = onceWithCleanup(bot, 'switchWorld', {
     timeout: 15000
   })
+
+  let switchWorldTriggered = false
+  
+  const switchWorldListener = () => {
+    switchWorldTriggered = true
+  }
+
+  bot.on('switchWorld', switchWorldListener)
+
+  bot.test.sayEverywhere('/kill')
+  
+  await sleep(3000)
+
+  bot.off('switchWorld', switchWorldListener)
+
+  if (switchWorldTriggered) {
+    throw new Error('switchWorld event was triggered after kill, not after dimension switch!')
+  }
 
   bot.test.sayEverywhere('/setblock ~ ~ ~ nether_portal')
   bot.test.sayEverywhere('/setblock ~ ~ ~ portal')
