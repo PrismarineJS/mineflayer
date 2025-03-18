0ccbcd71cfa66413249da01c0069dfdea4533d9c
Sinan <shuriken209master@googlemail.com>
2025-03-18 01:43:04 +0100

    .

--- test/externalTests/switchWorldEvent.js
+++ test/externalTests/switchWorldEvent.js
@@ -2,8 +2,10 @@ const { once, onceWithCleanup } = require('../../lib/promise_utils')
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports = () => async (bot) => {
+  bot.chat('switchWorldEvent test')
+  
   bot.on('switchWorld', () => {
-    bot.test.sayEverywhere('switchWorld event fired')
+    bot.chat('switchWorld event fired')
   })
 
   let switchWorldTriggered = false
@@ -14,26 +16,26 @@ module.exports = () => async (bot) => {
   const switchWorldListener = () => {
     switchWorldTriggered = true
   }
-  bot.test.sayEverywhere('/gamemode creative')
-  bot.test.sayEverywhere('/gamemode 1')
+  bot.chat('/gamemode creative')
+  bot.chat('/gamemode 1')
 
   await sleep(500)
   bot.on('switchWorld', switchWorldListener)
-  bot.test.sayEverywhere('Killing now')
-  bot.test.sayEverywhere('/kill')
+  bot.chat('Killing now')
+  bot.chat('/kill')
   await sleep(3000)
   bot.off('switchWorld', switchWorldListener)
-  bot.test.sayEverywhere('Kill switchWorld time over')
+  bot.chat('Kill switchWorld time over')
 
   if (switchWorldTriggered) {
     throw new Error('switchWorld event was triggered after kill, not after dimension switch!')
   }
 
   await sleep(1000)
-  bot.test.sayEverywhere('/setblock ~ ~ ~ nether_portal')
-  bot.test.sayEverywhere('/setblock ~ ~ ~ portal')
+  bot.chat('/setblock ~ ~ ~ nether_portal')
+  bot.chat('/setblock ~ ~ ~ portal')
 
   await switchWorldPromise
 
-  bot.test.sayEverywhere('Bot switched worlds, switchWorld event was properly emitted!')
+  bot.chat('Bot switched worlds, switchWorld event was properly emitted!')
 }
