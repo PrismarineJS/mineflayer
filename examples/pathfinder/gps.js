4bc0e0adfbe1162650a8f09b4449216b2f1e8aed
GenerelSchwerz <xXSG.KingXx@gmail.com>
2026-02-21 15:10:32 -0800

    fix support for pathfinder, aka backwards compatibility

--- examples/pathfinder/gps.js
+++ examples/pathfinder/gps.js
@@ -1,40 +1,57 @@
 // This is an example that uses mineflayer-pathfinder to showcase how simple it is to walk to goals
 
-const mineflayer = require('../../')
-const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
+const mineflayer = require("../../");
+const {
+  pathfinder,
+  Movements,
+  goals: { GoalNear },
+} = require("mineflayer-pathfinder");
 
 if (process.argv.length < 4 || process.argv.length > 6) {
-  console.log('Usage : node gps.js <host> <port> [<name>] [<password>]')
-  process.exit(1)
+  console.log("Usage : node gps.js <host> <port> [<name>] [<password>]");
+  process.exit(1);
 }
 
 const bot = mineflayer.createBot({
   host: process.argv[2],
   port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'gps',
-  password: process.argv[5]
-})
+  username: process.argv[4] ? process.argv[4] : "gps",
+  password: process.argv[5],
+});
 
-const RANGE_GOAL = 1 // get within this radius of the player
+const RANGE_GOAL = 1; // get within this radius of the player
 
-bot.loadPlugin(pathfinder)
+bot.loadPlugin(pathfinder);
 
-bot.once('spawn', () => {
-  const defaultMove = new Movements(bot)
+bot.once("spawn", () => {
+  const defaultMove = new Movements(bot);
 
-  bot.on('chat', (username, message) => {
-    if (username === bot.username) return
-    if (message !== 'come') return
-    const target = bot.players[username]?.entity
-    if (!target) {
-      bot.chat("I don't see you !")
-      return
+  bot.on("chat", (username, message) => {
+    console.log("Chat message received: ", username, message);
+    if (username === bot.username) return;
+
+    const [cmd, ...args] = message.split(" ");
+
+  const target = bot.players[username]?.entity;
+          if (!target) {
+            bot.chat("I don't see you !");
+            return;
+          }
+
+    switch (cmd) {
+      case "come": {
+       
+        const { x: playerX, y: playerY, z: playerZ } = target.position;
+
+        bot.pathfinder.setMovements(defaultMove);
+        bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL));
+      }
+      case "forward": {
+        bot.lookAt(target.position);
+        bot.setControlState("forward", true)
+      }
     }
-    const { x: playerX, y: playerY, z: playerZ } = target.position
+  });
+});
 
-    bot.pathfinder.setMovements(defaultMove)
-    bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
-  })
-})
-
-bot.on('error', err => console.log(err))
+bot.on("error", (err) => console.log(err));
