018c55087391f17d35f792932d92c4afbfbe5fbd
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2026-09-05 17:08:44 -0700

    increase speed of tests exampleBlockFinder.js and exampleDigger.js
    removes a redundant two-second delay after the child has already confirmed its chunks are loaded.
    replaces its two-second guess with an inventory response that synchronizes /op and /give.

--- test/externalTests/exampleDigger.js
+++ test/externalTests/exampleDigger.js
@@ -3,9 +3,12 @@ const assert = require('assert')
 module.exports = () => async (bot) => {
   await bot.test.runExample('examples/digger.js', async (name) => {
     assert.strictEqual(name, 'digger')
-    bot.chat('/op digger') // to counteract spawn protection
-    bot.chat('/give digger dirt 64')
-    await bot.test.wait(2000)
+    bot.chat(`/op ${name}`) // to counteract spawn protection
+    bot.chat(`/give ${name} dirt 64`)
+    await bot.test.tellAndListen(name, 'list', (message) => {
+      assert.match(message, /(?:^|, )dirt x 64(?:, |$)/)
+      return true
+    })
     await bot.test.tellAndListen(name, 'dig', (message) => {
       if (message.startsWith('starting')) {
         return false // continue to listen
