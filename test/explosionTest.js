57b7d4f29659af4ee467c8d41cc5fcf7a471a99a
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-18 18:44:13 +0800

    test: cover explosion damage for non-player entities

---
+++ test/explosionTest.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+const { Vec3 } = require('vec3')
+const injectExplosion = require('../lib/plugins/explosion')
+
+function createEntity (width, height) {
+  return {
+    position: new Vec3(0, 0, 0),
+    width,
+    height
+  }
+}
+
+describe('explosion damage', function () {
+  it('uses the target entity dimensions when calculating exposure', function () {
+    const bot = {
+      world: {
+        raycast: (origin, direction) => direction.y > 0.3 ? {} : null
+      }
+    }
+    injectExplosion(bot)
+
+    const source = new Vec3(0, 0, -2)
+    const smallEntityDamage = bot.getExplosionDamages(createEntity(0.4, 0.7), source, 6, true)
+    const playerSizedEntityDamage = bot.getExplosionDamages(createEntity(0.6, 1.8), source, 6, true)
+
+    assert.notStrictEqual(smallEntityDamage, playerSizedEntityDamage)
+  })
+}
